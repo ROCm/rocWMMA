65518894aa553e7639864f9d58aa5e1abc923e49
Chris Millette <chris.millette@amd.com>
2021-01-29 18:55:34 -0500

    Implement validation for matrix mult. Initial tests for mfma_32_32_k_fp32

--- MmaSyncTest.cpp
+++ MmaSyncTest.cpp
@@ -87,7 +87,7 @@ __global__ void test_mma_sync_d(const InputT* a,
                             + (std::is_same<LayoutC, row_major>::value ? (cRow * ldc + cCol)
                                                                        : (cRow + cCol * ldc));
         wmma::load_matrix_sync(fragC,
-                               c + cRow + cCol * ldc,
+                               cOffset,
                                ldc,
                                std::is_same<LayoutC, row_major>::value ? wmma::mem_row_major
                                                                        : wmma::mem_col_major);
@@ -99,7 +99,7 @@ __global__ void test_mma_sync_d(const InputT* a,
 
         // Store the output
         wmma::store_matrix_sync(cOffset,
-                                fragAcc,
+                                fragC,
                                 ldc,
                                 std::is_same<LayoutC, row_major>::value ? wmma::mem_row_major
                                                                         : wmma::mem_col_major);
@@ -118,7 +118,14 @@ template <uint32_t TBlockX,
           typename LayoutC>
 __host__ void test_mma_sync_h(uint32_t M, uint32_t N, uint32_t K, ComputeT alpha, ComputeT beta)
 {
-    std::cout << "HIP matrix mult test\n";
+    std::cout << "HIP wmma::mma_sync test: TBlock (" << TBlockX << ", " << TBlockY
+              << ") "
+              << "BlockMNK(" << BlockM << ", " << BlockN << ", " << BlockK << ") "
+              << "MatrixMNK(" << M << ", " << N << ", " << K << ") "
+              << "FmtABC(" << (std::is_same<LayoutA, row_major>::value ? "R" : "C") << ", "
+              << (std::is_same<LayoutB, row_major>::value ? "R" : "C") << ", "
+              << (std::is_same<LayoutC, row_major>::value ? "R" : "C") << ") "
+              << "TiTc(" << (std::is_same<InputT, float32_t>::value ? "f32" : "X") << ") \n";
 
     int lda = std::is_same<LayoutA, row_major>::value ? K : M;
     int ldb = std::is_same<LayoutB, row_major>::value ? N : K;
@@ -175,189 +182,86 @@ __host__ void test_mma_sync_h(uint32_t M, uint32_t N, uint32_t K, ComputeT alpha
     assert(hipFree(d_a) == hipSuccess);
     assert(hipFree(d_b) == hipSuccess);
     assert(hipFree(d_c) == hipSuccess);
+
+    // Validate
+    std::vector<ComputeT> matrixC_r(M * N, 0.0f);
+    gemmCPU<LayoutA, LayoutB, LayoutC, InputT, ComputeT>(
+        matrixA,
+        matrixB,
+        matrixC_r,
+        M,
+        N,
+        K,
+        alpha,
+        beta);
+    compareEqual<InputT, InputT, LayoutA, LayoutA>(matrixC, matrixC_r, M, N);
+}
+
+template <uint32_t TBlockX,
+          uint32_t TBlockY,
+          uint32_t BlockM,
+          uint32_t BlockN,
+          uint32_t BlockK,
+          typename InputT,
+          typename ComputeT>
+inline void test_mma_sync_h(uint32_t M, uint32_t N, uint32_t K, ComputeT alpha, ComputeT beta)
+{
+    test_mma_sync_h<TBlockX, TBlockY, BlockM, BlockN, BlockK, float32_t, float32_t, row_major, row_major, row_major>(M, N, K, alpha, beta);
+    test_mma_sync_h<TBlockX, TBlockY, BlockM, BlockN, BlockK, float32_t, float32_t, row_major, col_major, row_major>(M, N, K, alpha, beta);
+    test_mma_sync_h<TBlockX, TBlockY, BlockM, BlockN, BlockK, float32_t, float32_t, col_major, row_major, row_major>(M, N, K, alpha, beta);
+    test_mma_sync_h<TBlockX, TBlockY, BlockM, BlockN, BlockK, float32_t, float32_t, col_major, col_major, row_major>(M, N, K, alpha, beta);
+    test_mma_sync_h<TBlockX, TBlockY, BlockM, BlockN, BlockK, float32_t, float32_t, row_major, row_major, col_major>(M, N, K, alpha, beta);
+    test_mma_sync_h<TBlockX, TBlockY, BlockM, BlockN, BlockK, float32_t, float32_t, row_major, col_major, col_major>(M, N, K, alpha, beta);
+    test_mma_sync_h<TBlockX, TBlockY, BlockM, BlockN, BlockK, float32_t, float32_t, col_major, row_major, col_major>(M, N, K, alpha, beta);
+    test_mma_sync_h<TBlockX, TBlockY, BlockM, BlockN, BlockK, float32_t, float32_t, col_major, col_major, col_major>(M, N, K, alpha, beta);
+}
+
+template<uint32_t TBlockX,
+          uint32_t TBlockY,
+          typename InputT,
+          typename ComputeT>
+inline void test_mma_sync_h_32x32(uint32_t M, uint32_t N, uint32_t K, ComputeT alpha, ComputeT beta)
+{
+    test_mma_sync_h<TBlockX, TBlockY, 32, 32, 2, InputT, ComputeT>(M, N, K, alpha, beta);
+    test_mma_sync_h<TBlockX, TBlockY, 32, 32, 4, InputT, ComputeT>(M, N, K, alpha, beta);
+    test_mma_sync_h<TBlockX, TBlockY, 32, 32, 8, InputT, ComputeT>(M, N, K, alpha, beta);
+    test_mma_sync_h<TBlockX, TBlockY, 32, 32, 16, InputT, ComputeT>(M, N, K, alpha, beta);
+    test_mma_sync_h<TBlockX, TBlockY, 32, 32, 32, InputT, ComputeT>(M, N, K, alpha, beta);
+    test_mma_sync_h<TBlockX, TBlockY, 32, 32, 64, InputT, ComputeT>(M, N, K, alpha, beta);
+    test_mma_sync_h<TBlockX, TBlockY, 32, 32, 128, InputT, ComputeT>(M, N, K, alpha, beta);
+    test_mma_sync_h<TBlockX, TBlockY, 32, 32, 256, InputT, ComputeT>(M, N, K, alpha, beta);
+    test_mma_sync_h<TBlockX, TBlockY, 32, 32, 512, InputT, ComputeT>(M, N, K, alpha, beta);
+    test_mma_sync_h<TBlockX, TBlockY, 32, 32, 1024, InputT, ComputeT>(M, N, K, alpha, beta);
 }
 
 void test_mma_sync_h()
 {
-    // For fills, we must have the same geometry for all matrices
+    // float32_t  64 x 1 threads, block 32 x 32 x 2/4/8/16/32/64/128/256/512/1024,
+    test_mma_sync_h_32x32<64, 1, float32_t, float32_t>(64, 64, 1024, 2.0f, 2.0f);
+    test_mma_sync_h_32x32<64, 1, float32_t, float32_t>(32, 64, 1024, 2.0f, 2.0f);
+    test_mma_sync_h_32x32<64, 1, float32_t, float32_t>(64, 32, 1024, 2.0f, 2.0f);
 
-    // float32_t  64 x 1 threads, block 64 x 64,
+    test_mma_sync_h_32x32<64, 1, float32_t, float32_t>(1024, 2048, 1024, 2.0f, 2.0f);
+    test_mma_sync_h_32x32<64, 1, float32_t, float32_t>(2048, 64, 1024, 2.0f, 2.0f);
+    test_mma_sync_h_32x32<64, 1, float32_t, float32_t>(2048, 2048, 1024, 2.0f, 2.0f);
 
-    // MNK = 64
-    test_mma_sync_h<64, 1, 32, 32, 32, float32_t, float32_t, row_major, row_major, row_major>(
-        64, 64, 64, 1.0f, 0.0f);
-    // test_fill_fragment_h<64, 1, 64, 64, 64, float32_t, float32_t, row_major, col_major, row_major>
-    // (64, 64, 64, 4.0f, -5.0f, 6.0f);
-    // test_fill_fragment_h<64, 1, 64, 64, 64, float32_t, float32_t, col_major, row_major, row_major>
-    // (64, 64, 64, -7.0f, 8.0f, -9.0f);
-    // test_fill_fragment_h<64, 1, 64, 64, 64, float32_t, float32_t, col_major, col_major, row_major>
-    // (64, 64, 64, 10.0f, -11.0f, 12.0f);
+    // float32_t  64 x 2 threads, block 32 x 32 x 2/4/8/16/32/64/128/256/512/1024,
+    test_mma_sync_h_32x32<64, 2, float32_t, float32_t>(64, 64, 1024, 2.0f, 2.0f);
+    test_mma_sync_h_32x32<64, 2, float32_t, float32_t>(128, 64, 1024, 2.0f, 2.0f);
+    test_mma_sync_h_32x32<64, 2, float32_t, float32_t>(64, 128, 1024, 2.0f, 2.0f);
 
-    // // MNK = 128
-    // test_fill_fragment_h<64, 1, 64, 64, 64, float32_t, float32_t, row_major, row_major, row_major>
-    // (128, 128, 128, -1.0f, 2.0f, -3.0f);
-    // test_fill_fragment_h<64, 1, 64, 64, 64, float32_t, float32_t, row_major, col_major, row_major>
-    // (128, 128, 128, 4.0f, -5.0f, 6.0f);
-    // test_fill_fragment_h<64, 1, 64, 64, 64, float32_t, float32_t, col_major, row_major, row_major>
-    // (128, 128, 128, -7.0f, 8.0f, -9.0f);
-    // test_fill_fragment_h<64, 1, 64, 64, 64, float32_t, float32_t, col_major, col_major, row_major>
-    // (128, 128, 128, 10.0f, -11.0f, 12.0f);
+    test_mma_sync_h_32x32<64, 2, float32_t, float32_t>(1024, 2048, 1024, 2.0f, 2.0f);
+    test_mma_sync_h_32x32<64, 2, float32_t, float32_t>(2048, 64, 1024, 2.0f, 2.0f);
+    test_mma_sync_h_32x32<64, 2, float32_t, float32_t>(2048, 2048, 1024, 2.0f, 2.0f);
 
-    // // MNK = 512
-    // test_fill_fragment_h<64, 1, 64, 64, 64, float32_t, float32_t, row_major, row_major, row_major>
-    // (512, 512, 512, -1.0f, 2.0f, -3.0f);
-    // test_fill_fragment_h<64, 1, 64, 64, 64, float32_t, float32_t, row_major, col_major, row_major>
-    // (512, 512, 512, 4.0f, -5.0f, 6.0f);
-    // test_fill_fragment_h<64, 1, 64, 64, 64, float32_t, float32_t, col_major, row_major, row_major>
-    // (512, 512, 512, -7.0f, 8.0f, -9.0f);
-    // test_fill_fragment_h<64, 1, 64, 64, 64, float32_t, float32_t, col_major, col_major, row_major>
-    // (512, 512, 512, 10.0f, -11.0f, 12.0f);
+    // float32_t  64 x 4 threads, block 32 x 32 x 2/4/8/16/32/64/128/256/512/1024,
+    test_mma_sync_h_32x32<64, 4, float32_t, float32_t>(128, 128, 1024, 2.0f, 2.0f);
+    test_mma_sync_h_32x32<64, 4, float32_t, float32_t>(128, 256, 1024, 2.0f, 2.0f);
+    test_mma_sync_h_32x32<64, 4, float32_t, float32_t>(256, 128, 1024, 2.0f, 2.0f);
 
-    // // MNK = 16384
-    // test_fill_fragment_h<64, 1, 64, 64, 64, float32_t, float32_t, row_major, row_major, row_major>
-    // (16384, 16384, 16384, -1.0f, 2.0f, -3.0f);
-    // test_fill_fragment_h<64, 1, 64, 64, 64, float32_t, float32_t, row_major, col_major, row_major>
-    // (16384, 16384, 16384, 4.0f, -5.0f, 6.0f);
-    // test_fill_fragment_h<64, 1, 64, 64, 64, float32_t, float32_t, col_major, row_major, row_major>
-    // (16384, 16384, 16384, -7.0f, 8.0f, -9.0f);
-    // test_fill_fragment_h<64, 1, 64, 64, 64, float32_t, float32_t, col_major, col_major, row_major>
-    // (16384, 16384, 16384, 10.0f, -11.0f, 12.0f);
-
-    // // float32_t  64 x 2 threads, block 64 x 64,
-
-    // // MNK = 128
-    // test_fill_fragment_h<64, 2, 64, 64, 64, float32_t, float32_t, row_major, row_major, row_major>
-    // (128, 128, 128, -1.0f, 2.0f, -3.0f);
-    // test_fill_fragment_h<64, 2, 64, 64, 64, float32_t, float32_t, row_major, col_major, row_major>
-    // (128, 128, 128, 4.0f, -5.0f, 6.0f);
-    // test_fill_fragment_h<64, 2, 64, 64, 64, float32_t, float32_t, col_major, row_major, row_major>
-    // (128, 128, 128, -7.0f, 8.0f, -9.0f);
-    // test_fill_fragment_h<64, 2, 64, 64, 64, float32_t, float32_t, col_major, col_major, row_major>
-    // (128, 128, 128, 10.0f, -11.0f, 12.0f);
-
-    // // MNK = 512
-    // test_fill_fragment_h<64, 2, 64, 64, 64, float32_t, float32_t, row_major, row_major, row_major>
-    // (512, 512, 512, -1.0f, 2.0f, -3.0f);
-    // test_fill_fragment_h<64, 2, 64, 64, 64, float32_t, float32_t, row_major, col_major, row_major>
-    // (512, 512, 512, 4.0f, -5.0f, 6.0f);
-    // test_fill_fragment_h<64, 2, 64, 64, 64, float32_t, float32_t, col_major, row_major, row_major>
-    // (512, 512, 512, -7.0f, 8.0f, -9.0f);
-    // test_fill_fragment_h<64, 2, 64, 64, 64, float32_t, float32_t, col_major, col_major, row_major>
-    // (512, 512, 512, 10.0f, -11.0f, 12.0f);
-
-    // // MNK = 16384
-    // test_fill_fragment_h<64, 2, 64, 64, 64, float32_t, float32_t, row_major, row_major, row_major>
-    // (16384, 16384, 16384, -1.0f, 2.0f, -3.0f);
-    // test_fill_fragment_h<64, 2, 64, 64, 64, float32_t, float32_t, row_major, col_major, row_major>
-    // (16384, 16384, 16384, 4.0f, -5.0f, 6.0f);
-    // test_fill_fragment_h<64, 2, 64, 64, 64, float32_t, float32_t, col_major, row_major, row_major>
-    // (16384, 16384, 16384, -7.0f, 8.0f, -9.0f);
-    // test_fill_fragment_h<64, 2, 64, 64, 64, float32_t, float32_t, col_major, col_major, row_major>
-    // (16384, 16384, 16384, 10.0f, -11.0f, 12.0f);
-
-    // // float32_t  64 x 4 threads, block 64 x 64,
-
-    // // MNK = 512
-    // test_fill_fragment_h<64, 4, 64, 64, 64, float32_t, float32_t, row_major, row_major, row_major>
-    // (512, 512, 512, -1.0f, 2.0f, -3.0f);
-    // test_fill_fragment_h<64, 4, 64, 64, 64, float32_t, float32_t, row_major, col_major, row_major>
-    // (512, 512, 512, 4.0f, -5.0f, 6.0f);
-    // test_fill_fragment_h<64, 4, 64, 64, 64, float32_t, float32_t, col_major, row_major, row_major>
-    // (512, 512, 512, -7.0f, 8.0f, -9.0f);
-    // test_fill_fragment_h<64, 4, 64, 64, 64, float32_t, float32_t, col_major, col_major, row_major>
-    // (512, 512, 512, 10.0f, -11.0f, 12.0f);
-
-    // // float32_t  64 x 8 threads, block 64 x 64,
-
-    // // MNK = 512
-    // test_fill_fragment_h<64, 8, 64, 64, 64, float32_t, float32_t, row_major, row_major, row_major>
-    // (512, 512, 512, -1.0f, 2.0f, -3.0f);
-    // test_fill_fragment_h<64, 8, 64, 64, 64, float32_t, float32_t, row_major, col_major, row_major>
-    // (512, 512, 512, 4.0f, -5.0f, 6.0f);
-    // test_fill_fragment_h<64, 8, 64, 64, 64, float32_t, float32_t, col_major, row_major, row_major>
-    // (512, 512, 512, -7.0f, 8.0f, -9.0f);
-    // test_fill_fragment_h<64, 8, 64, 64, 64, float32_t, float32_t, col_major, col_major, row_major>
-    // (512, 512, 512, 10.0f, -11.0f, 12.0f);
-
-    // // float32_t  128 x 1 threads, block 64 x 64,
-
-    // // MNK = 128
-    // test_fill_fragment_h<128, 1, 64, 64, 64, float32_t, float32_t, row_major, row_major, row_major>
-    // (128, 128, 128, -1.0f, 2.0f, -3.0f);
-    // test_fill_fragment_h<128, 1, 64, 64, 64, float32_t, float32_t, row_major, col_major, row_major>
-    // (128, 128, 128, 4.0f, -5.0f, 6.0f);
-    // test_fill_fragment_h<128, 1, 64, 64, 64, float32_t, float32_t, col_major, row_major, row_major>
-    // (128, 128, 128, -7.0f, 8.0f, -9.0f);
-    // test_fill_fragment_h<128, 1, 64, 64, 64, float32_t, float32_t, col_major, col_major, row_major>
-    // (128, 128, 128, 10.0f, -11.0f, 12.0f);
-
-    // // MNK = 512
-    // test_fill_fragment_h<128, 1, 64, 64, 64, float32_t, float32_t, row_major, row_major, row_major>
-    // (512, 512, 512, -1.0f, 2.0f, -3.0f);
-    // test_fill_fragment_h<128, 1, 64, 64, 64, float32_t, float32_t, row_major, col_major, row_major>
-    // (512, 512, 512, 4.0f, -5.0f, 6.0f);
-    // test_fill_fragment_h<128, 1, 64, 64, 64, float32_t, float32_t, col_major, row_major, row_major>
-    // (512, 512, 512, -7.0f, 8.0f, -9.0f);
-    // test_fill_fragment_h<128, 1, 64, 64, 64, float32_t, float32_t, col_major, col_major, row_major>
-    // (512, 512, 512, 10.0f, -11.0f, 12.0f);
-
-    // // float32_t  128 x 2 threads, block 64 x 64,
-
-    // // MNK = 128
-    // test_fill_fragment_h<128, 2, 64, 64, 64, float32_t, float32_t, row_major, row_major, row_major>
-    // (128, 128, 128, -1.0f, 2.0f, -3.0f);
-    // test_fill_fragment_h<128, 2, 64, 64, 64, float32_t, float32_t, row_major, col_major, row_major>
-    // (128, 128, 128, 4.0f, -5.0f, 6.0f);
-    // test_fill_fragment_h<128, 2, 64, 64, 64, float32_t, float32_t, col_major, row_major, row_major>
-    // (128, 128, 128, -7.0f, 8.0f, -9.0f);
-    // test_fill_fragment_h<128, 2, 64, 64, 64, float32_t, float32_t, col_major, col_major, row_major>
-    // (128, 128, 128, 10.0f, -11.0f, 12.0f);
-
-    // // MNK = 512
-    // test_fill_fragment_h<128, 2, 64, 64, 64, float32_t, float32_t, row_major, row_major, row_major>
-    // (512, 512, 512, -1.0f, 2.0f, -3.0f);
-    // test_fill_fragment_h<128, 2, 64, 64, 64, float32_t, float32_t, row_major, col_major, row_major>
-    // (512, 512, 512, 4.0f, -5.0f, 6.0f);
-    // test_fill_fragment_h<128, 2, 64, 64, 64, float32_t, float32_t, col_major, row_major, row_major>
-    // (512, 512, 512, -7.0f, 8.0f, -9.0f);
-    // test_fill_fragment_h<128, 2, 64, 64, 64, float32_t, float32_t, col_major, col_major, row_major>
-    // (512, 512, 512, 10.0f, -11.0f, 12.0f);
-
-    // // float32_t  128 x 4 threads, block 64 x 64,
-
-    // // MNK = 512
-    // test_fill_fragment_h<128, 4, 64, 64, 64, float32_t, float32_t, row_major, row_major, row_major>
-    // (512, 512, 512, -1.0f, 2.0f, -3.0f);
-    // test_fill_fragment_h<128, 4, 64, 64, 64, float32_t, float32_t, row_major, col_major, row_major>
-    // (512, 512, 512, 4.0f, -5.0f, 6.0f);
-    // test_fill_fragment_h<128, 4, 64, 64, 64, float32_t, float32_t, col_major, row_major, row_major>
-    // (512, 512, 512, -7.0f, 8.0f, -9.0f);
-    // test_fill_fragment_h<128, 4, 64, 64, 64, float32_t, float32_t, col_major, col_major, row_major>
-    // (512, 512, 512, 10.0f, -11.0f, 12.0f);
-
-    // // float32_t  256 x 1 threads, block 64 x 64,
-
-    // // MNK = 512
-    // test_fill_fragment_h<256, 1, 64, 64, 64, float32_t, float32_t, row_major, row_major, row_major>
-    // (512, 512, 512, -1.0f, 2.0f, -3.0f);
-    // test_fill_fragment_h<256, 1, 64, 64, 64, float32_t, float32_t, row_major, col_major, row_major>
-    // (512, 512, 512, 4.0f, -5.0f, 6.0f);
-    // test_fill_fragment_h<256, 1, 64, 64, 64, float32_t, float32_t, col_major, row_major, row_major>
-    // (512, 512, 512, -7.0f, 8.0f, -9.0f);
-    // test_fill_fragment_h<256, 1, 64, 64, 64, float32_t, float32_t, col_major, col_major, row_major>
-    // (512, 512, 512, 10.0f, -11.0f, 12.0f);
-
-    // // float32_t  256 x 2 threads, block 64 x 64,
-
-    // // MNK = 512
-    // test_fill_fragment_h<256, 2, 64, 64, 64, float32_t, float32_t, row_major, row_major, row_major>
-    // (512, 512, 512, -1.0f, 2.0f, -3.0f);
-    // test_fill_fragment_h<256, 2, 64, 64, 64, float32_t, float32_t, row_major, col_major, row_major>
-    // (512, 512, 512, 4.0f, -5.0f, 6.0f);
-    // test_fill_fragment_h<256, 2, 64, 64, 64, float32_t, float32_t, col_major, row_major, row_major>
-    // (512, 512, 512, -7.0f, 8.0f, -9.0f);
-    // test_fill_fragment_h<256, 2, 64, 64, 64, float32_t, float32_t, col_major, col_major, row_major>
-    // (512, 512, 512, 10.0f, -11.0f, 12.0f);
+    test_mma_sync_h_32x32<64, 4, float32_t, float32_t>(1024, 2048, 1024, 2.0f, 2.0f);
+    test_mma_sync_h_32x32<64, 4, float32_t, float32_t>(2048, 128, 1024, 2.0f, 2.0f);
+    test_mma_sync_h_32x32<64, 4, float32_t, float32_t>(2048, 2048, 1024, 2.0f, 2.0f);
 }
 
 int main()
