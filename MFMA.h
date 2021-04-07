1be70a01e6d72902677bd10f460df24da9121f6c
Chris Millette <chris.millette@amd.com>
2021-04-07 18:15:52 -0400

    Add support for fp16 computeT and fix fp16 epsilon calc
    [ROCm/rocwmma commit: e773c064c440550fd0ae32d9878541fb31f62a2a]

--- MFMA.h
+++ MFMA.h
@@ -1,7 +1,10 @@
 #ifndef WMMA_MFMA_H
 #define WMMA_MFMA_H
 
+#include "Convert.h"
+#include "IOPack.h"
 #include "IOTraits.h"
+#include "IOUnpack.h"
 #include "Types.h"
 
 template <typename InputT, typename ComputeT, uint32_t BlockM, uint32_t BlockN>
@@ -13,7 +16,7 @@ struct amdgcn_mfma<float16_t, float32_t, 16, 16>
     // Packed register traits
     struct Traits
     {
-        enum : uint32_t 
+        enum : uint32_t
         {
             KPerMfma = 16,
         };
@@ -23,12 +26,48 @@ struct amdgcn_mfma<float16_t, float32_t, 16, 16>
         using DRegsT = AccRegF32x4;
     };
 
-    __device__ static inline auto exec(
-        typename Traits::ARegsT const& regsA,
-        typename Traits::BRegsT const& regsB,
-        typename Traits::CRegsT const& regsC) -> typename Traits::DRegsT
+    __device__ static inline auto exec(typename Traits::ARegsT const& regsA,
+                                       typename Traits::BRegsT const& regsB,
+                                       typename Traits::CRegsT const& regsC) ->
+        typename Traits::DRegsT
     {
-        return typename Traits::DRegsT(__builtin_amdgcn_mfma_f32_16x16x16f16(*regsA, *regsB, *regsC, 0, 0, 0));
+        return typename Traits::DRegsT(
+            __builtin_amdgcn_mfma_f32_16x16x16f16(*regsA, *regsB, *regsC, 0, 0, 0));
+    }
+};
+
+template <>
+struct amdgcn_mfma<float16_t, float16_t, 16, 16>
+{
+    // Packed register traits
+    struct Traits
+    {
+        enum : uint32_t
+        {
+            KPerMfma = 16,
+        };
+        using ARegsT = VRegF32x2;
+        using BRegsT = VRegF32x2;
+        using CRegsT = AccRegF32x2;
+        using DRegsT = AccRegF32x2;
+    };
+
+    __device__ static inline auto exec(typename Traits::ARegsT const& regsA,
+                                       typename Traits::BRegsT const& regsB,
+                                       typename Traits::CRegsT const& regsC) ->
+        typename Traits::DRegsT
+    {
+        using Mfma              = amdgcn_mfma<float16_t, float32_t, 16, 16>;
+        using UnpackC           = Unpack<float16_t, 2>;
+        using PackD             = Pack<float16_t, 4>;
+        using Convert_fp16_fp32 = amdgcn_convert<float16_t, float32_t>;
+        using Convert_fp32_fp16 = amdgcn_convert<float32_t, float16_t>;
+
+        // MFMA unit compute type is always fp32.
+        // Upconvert C to fp32, do MFMA, then down convert D
+        // to fp16 as 'simulated' fp16 computation
+        auto Dfp32 = Mfma::exec(regsA, regsB, Convert_fp16_fp32::exec(UnpackC::exec(regsC)));
+        return PackD::exec(Convert_fp32_fp16::exec(Dfp32));
     }
 };
 
@@ -38,7 +77,7 @@ struct amdgcn_mfma<float16_t, float32_t, 32, 32>
     // Packed register traits
     struct Traits
     {
-        enum : uint32_t 
+        enum : uint32_t
         {
             KPerMfma = 8
         };
@@ -48,12 +87,47 @@ struct amdgcn_mfma<float16_t, float32_t, 32, 32>
         using DRegsT = AccRegF32x16;
     };
 
-    __device__ static inline auto exec(
-        typename Traits::ARegsT const& regsA,
-        typename Traits::BRegsT const& regsB,
-        typename Traits::CRegsT const& regsC) -> typename Traits::DRegsT
+    __device__ static inline auto exec(typename Traits::ARegsT const& regsA,
+                                       typename Traits::BRegsT const& regsB,
+                                       typename Traits::CRegsT const& regsC) ->
+        typename Traits::DRegsT
     {
-        return typename Traits::DRegsT(__builtin_amdgcn_mfma_f32_32x32x8f16(*regsA, *regsB, *regsC, 0, 0, 0));
+        return typename Traits::DRegsT(
+            __builtin_amdgcn_mfma_f32_32x32x8f16(*regsA, *regsB, *regsC, 0, 0, 0));
+    }
+};
+
+template <>
+struct amdgcn_mfma<float16_t, float16_t, 32, 32>
+{
+    // Packed register traits
+    struct Traits
+    {
+        enum : uint32_t
+        {
+            KPerMfma = 8,
+        };
+        using ARegsT = VRegF32x2;
+        using BRegsT = VRegF32x2;
+        using CRegsT = AccRegF32x8;
+        using DRegsT = AccRegF32x8;
+    };
+
+    __device__ static inline auto exec(typename Traits::ARegsT const& regsA,
+                                       typename Traits::BRegsT const& regsB,
+                                       typename Traits::CRegsT const& regsC) ->
+        typename Traits::DRegsT
+    {
+        using Mfma              = amdgcn_mfma<float16_t, float32_t, 32, 32>;
+        using UnpackC           = Unpack<float16_t, 8>;
+        using PackD             = Pack<float16_t, 16>;
+        using Convert_fp16_fp32 = amdgcn_convert<float16_t, float32_t>;
+        using Convert_fp32_fp16 = amdgcn_convert<float32_t, float16_t>;
+
+        // MFMA unit compute type is always fp32.
+        // Upconvert C to fp32, do MFMA, then down convert D to fp16 result;
+        auto Dfp32 = Mfma::exec(regsA, regsB, Convert_fp16_fp32::exec(UnpackC::exec(regsC)));
+        return PackD::exec(Convert_fp32_fp16::exec(Dfp32));
     }
 };
 
@@ -63,7 +137,7 @@ struct amdgcn_mfma<float32_t, float32_t, 16, 16>
     // Packed register traits
     struct Traits
     {
-        enum : uint32_t 
+        enum : uint32_t
         {
             KPerMfma = 4
         };
@@ -73,12 +147,13 @@ struct amdgcn_mfma<float32_t, float32_t, 16, 16>
         using DRegsT = AccRegF32x4;
     };
 
-    __device__ static inline auto exec(
-        typename Traits::ARegsT const& regsA,
-        typename Traits::BRegsT const& regsB,
-        typename Traits::CRegsT const& regsC) -> typename Traits::DRegsT
+    __device__ static inline auto exec(typename Traits::ARegsT const& regsA,
+                                       typename Traits::BRegsT const& regsB,
+                                       typename Traits::CRegsT const& regsC) ->
+        typename Traits::DRegsT
     {
-        return typename Traits::DRegsT(__builtin_amdgcn_mfma_f32_16x16x4f32(*regsA, *regsB, *regsC, 0, 0, 0));
+        return typename Traits::DRegsT(
+            __builtin_amdgcn_mfma_f32_16x16x4f32(*regsA, *regsB, *regsC, 0, 0, 0));
     }
 };
 
@@ -89,7 +164,7 @@ struct amdgcn_mfma<float32_t, float32_t, 32, 32>
     // Packed register traits
     struct Traits
     {
-        enum : uint32_t 
+        enum : uint32_t
         {
             KPerMfma = 2
         };
@@ -99,12 +174,13 @@ struct amdgcn_mfma<float32_t, float32_t, 32, 32>
         using DRegsT = AccRegF32x16;
     };
 
-    __device__ static inline auto exec(
-        typename Traits::ARegsT const& regsA,
-        typename Traits::BRegsT const& regsB,
-        typename Traits::CRegsT const& regsC) -> typename Traits::DRegsT
+    __device__ static inline auto exec(typename Traits::ARegsT const& regsA,
+                                       typename Traits::BRegsT const& regsB,
+                                       typename Traits::CRegsT const& regsC) ->
+        typename Traits::DRegsT
     {
-        return typename Traits::DRegsT(__builtin_amdgcn_mfma_f32_32x32x2f32(*regsA, *regsB, *regsC, 0, 0, 0));
+        return typename Traits::DRegsT(
+            __builtin_amdgcn_mfma_f32_32x32x2f32(*regsA, *regsB, *regsC, 0, 0, 0));
     }
 };
 
@@ -114,34 +190,46 @@ struct amdgcn_mfma_MxNxK
     struct Traits
     {
         using MFMA = amdgcn_mfma<InputT, ComputeT, BlockM, BlockN>;
-        
+
         enum : uint32_t
         {
             MfmaCount = BlockK / MFMA::Traits::KPerMfma,
-            MinK = MFMA::Traits::KPerMfma,
+            MinK      = MFMA::Traits::KPerMfma,
         };
 
         // Propagate individual MFMA types to full block inputs.
-        using ARegsT = VecT<typename MFMA::Traits::ARegsT::Type, MfmaCount * MFMA::Traits::ARegsT::size()>;
-        using BRegsT = VecT<typename MFMA::Traits::BRegsT::Type, MfmaCount * MFMA::Traits::BRegsT::size()>;
+        using ARegsT
+            = VecT<typename MFMA::Traits::ARegsT::Type, MfmaCount * MFMA::Traits::ARegsT::size()>;
+        using BRegsT
+            = VecT<typename MFMA::Traits::BRegsT::Type, MfmaCount * MFMA::Traits::BRegsT::size()>;
         using CRegsT = VecT<typename MFMA::Traits::CRegsT::Type, MFMA::Traits::CRegsT::size()>;
         using DRegsT = VecT<typename MFMA::Traits::DRegsT::Type, MFMA::Traits::DRegsT::size()>;
 
         // Sanity checks
         static_assert(BlockK >= MinK, "BlockK is not a minimum of MinK");
         static_assert(BlockK % MinK == 0, "BlockK is not a multiple of MinK");
-        static_assert(std::is_same<ARegsT, BRegsT>::value, "A and B registers must be of same type");
-        static_assert(std::is_same<CRegsT, DRegsT>::value, "C and D registers must be of same type");
-        static_assert(ARegsT::size() == amdgcn_io_traits<BlockM, BlockK, InputT>::PackedRegisterCount, "Unexpected packed register count for A");
-        static_assert(BRegsT::size() == amdgcn_io_traits<BlockN, BlockK, InputT>::PackedRegisterCount, "Unexpected packed register count for B");
-        static_assert(CRegsT::size() == amdgcn_io_traits<BlockM, BlockN, ComputeT>::PackedRegisterCount, "Unexpected packed register count for C");
-        static_assert(DRegsT::size() == amdgcn_io_traits<BlockM, BlockN, ComputeT>::PackedRegisterCount, "Unexpected packed register count for D");
+        static_assert(std::is_same<ARegsT, BRegsT>::value,
+                      "A and B registers must be of same type");
+        static_assert(std::is_same<CRegsT, DRegsT>::value,
+                      "C and D registers must be of same type");
+        static_assert(ARegsT::size()
+                          == amdgcn_io_traits<BlockM, BlockK, InputT>::PackedRegisterCount,
+                      "Unexpected packed register count for A");
+        static_assert(BRegsT::size()
+                          == amdgcn_io_traits<BlockN, BlockK, InputT>::PackedRegisterCount,
+                      "Unexpected packed register count for B");
+        static_assert(CRegsT::size()
+                          == amdgcn_io_traits<BlockM, BlockN, ComputeT>::PackedRegisterCount,
+                      "Unexpected packed register count for C");
+        static_assert(DRegsT::size()
+                          == amdgcn_io_traits<BlockM, BlockN, ComputeT>::PackedRegisterCount,
+                      "Unexpected packed register count for D");
     };
 
-    __device__ static inline auto exec(
-        typename Traits::ARegsT const& regsA,
-        typename Traits::BRegsT const& regsB,
-        typename Traits::CRegsT const& regsC) -> typename Traits::DRegsT
+    __device__ static inline auto exec(typename Traits::ARegsT const& regsA,
+                                       typename Traits::BRegsT const& regsB,
+                                       typename Traits::CRegsT const& regsC) ->
+        typename Traits::DRegsT
     {
         typename Traits::DRegsT result = regsC;
 
--- Utils.h
+++ Utils.h
@@ -21,6 +21,25 @@ static constexpr intT1 ceilDiv(const intT1 numerator, const intT2 divisor)
     return (numerator + divisor - 1) / divisor;
 }
 
+template <typename DataT>
+struct MachineEpsilon
+{
+    static constexpr DataT value()
+    {
+        return std::numeric_limits<DataT>::epsilon();
+    }
+};
+
+template <>
+struct MachineEpsilon<float16_t>
+{
+    static float16_t value()
+    {
+        uint16_t eps = 0x1400;
+        return *reinterpret_cast<float16_t*>(&eps);
+    }
+};
+
 template <typename Layout>
 struct MatrixUtil
 {
@@ -75,32 +94,23 @@ struct MatrixUtil
     __host__ static inline void fill(std::vector<DataT>& mat, uint32_t m, uint32_t n, DataT value)
     {
         assert(mat.size() == n * m);
-
-        auto rowMjr = [](uint32_t row, uint32_t col, uint32_t ld) { return row * ld + col; };
-        auto colMjr = [](uint32_t row, uint32_t col, uint32_t ld) { return col * ld + row; };
-
-        auto index = std::is_same<Layout, row_major>::value ? rowMjr : colMjr;
-        auto ld    = std::is_same<Layout, row_major>::value ? n : m;
-
-        for(int i = 0; i < m; ++i) // row
+        for(int i = 0; i < m * n; ++i) // row
         {
-            for(int j = 0; j < n; ++j) // col
-            {
-                mat[index(i, j, ld)] = value;
-            }
+            mat[i] = value;
         }
     }
 };
 
 template <typename TypeA, typename TypeB, typename LayoutA, typename LayoutB>
-void compareEqual(std::vector<TypeA> const& a, std::vector<TypeB> const& b, int M, int N)
+void compareEqual(
+    std::vector<TypeA> const& a, std::vector<TypeB> const& b, int M, int N, double tolerance = 10.0)
 {
     assert(a.size() == b.size() && "A and B are not the same size");
     assert(a.size() == M * N && "A and B do not match size M x N");
     int lda = std::is_same<LayoutA, row_major>::value ? N : M;
     int ldb = std::is_same<LayoutB, row_major>::value ? N : M;
 
-    double max_relative_error = 0;
+    double max_relative_error = 0.0;
 
 #pragma omp parallel for
     for(int i = 0; i < M; ++i) // Row
@@ -110,7 +120,8 @@ void compareEqual(std::vector<TypeA> const& a, std::vector<TypeB> const& b, int 
             auto indexA = std::is_same<LayoutA, row_major>::value ? (i * lda + j) : (i + j * lda);
             auto indexB = std::is_same<LayoutB, row_major>::value ? (i * ldb + j) : (i + j * ldb);
 
-            auto relative_error = fabs(double(a[indexA] - b[indexB]) / a[indexA]);
+            auto relative_error
+                = a[indexA] != 0 ? fabs(double(a[indexA] - b[indexB]) / double(a[indexA])) : 0.0;
             if(relative_error > max_relative_error)
             {
                 max_relative_error = relative_error;
@@ -118,40 +129,47 @@ void compareEqual(std::vector<TypeA> const& a, std::vector<TypeB> const& b, int 
         }
     }
 
-    auto eps       = std::numeric_limits<TypeA>::epsilon();
-    auto tolerance = 10.0;
+    auto eps = MachineEpsilon<TypeA>::value();
     if(max_relative_error != max_relative_error || max_relative_error > eps * tolerance)
+    {
         std::cout << "FAIL: ";
+    }
     else
+    {
         std::cout << "PASS: ";
+    }
     std::cout << "max_relative_error = " << max_relative_error << std::endl;
 }
 
 template <typename DataT>
-void compareEqual(std::vector<DataT> const& a, std::vector<DataT> const& b, int M, int N)
+void compareEqual(
+    std::vector<DataT> const& a, std::vector<DataT> const& b, int M, int N, double tolerance = 10.0)
 {
     assert(a.size() == b.size() && "A and B are not the same size");
     assert(a.size() == M * N && "A and B do not match size M x N");
 
-    double   max_relative_error = 0;
+    double   max_relative_error = 0.0;
     uint32_t numElements        = M * N;
 
 #pragma omp parallel for
     for(int i = 0; i < numElements; ++i)
     {
-        auto relative_error = fabs(double(a[i] - b[i]) / a[i]);
+        auto relative_error = a[i] != 0 ? fabs(double(a[i] - b[i]) / double(a[i])) : 0.0;
         if(relative_error > max_relative_error)
         {
             max_relative_error = relative_error;
         }
     }
 
-    auto eps       = std::numeric_limits<DataT>::epsilon();
-    auto tolerance = 10.0;
+    auto eps = MachineEpsilon<DataT>::value();
     if(max_relative_error != max_relative_error || max_relative_error > eps * tolerance)
+    {
         std::cout << "FAIL: ";
+    }
     else
+    {
         std::cout << "PASS: ";
+    }
     std::cout << "max_relative_error = " << max_relative_error << std::endl;
 }
 
