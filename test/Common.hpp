9dcc22bff137f8495680e02b206aedbecf05cdb8
Anthony Chang <ac.chang@outlook.com>
2021-06-09 16:56:45 +0800

    temporarily skip tests that are affected by hipcc bug in half packing

--- test/Common.hpp
+++ test/Common.hpp
@@ -10,3 +10,42 @@ void for_each(std::tuple<Ts...>, F f) {
     std::initializer_list<int> _ = { (f(Ts{}), 0)... }; // poor man's fold expression for C++11/14
     // (f(Ts{}), ...); // fold expression is for C++17 only
 }
+
+namespace quirks
+{
+    // rocBLAS does not yet support Ti/To/Tc = bf16/bf16/bf16
+    template <typename InputT, typename OutputT, typename ComputeT>
+    struct rocblas_not_support_bf16_bf16_bf16 : std::false_type
+    {
+    };
+
+    template <>
+    struct rocblas_not_support_bf16_bf16_bf16<bfloat16_t, bfloat16_t, bfloat16_t> : std::true_type
+    {
+    };
+
+    // hipcc compiler currently has bug in half-type data packing around 'v_fma_mixlo_f16'
+    // where it forgot to zero out dest vgpr before writing into it
+    //
+    // currently the combination of column-major C/D matrix & half type output (fp16/bf16)
+    // is affected
+    template <typename OutputT,
+              typename LayoutC,
+              typename LayoutD = LayoutC,
+              typename Enable  = void>
+    struct hipcc_bug_half_packing : std::false_type
+    {
+    };
+
+    template <typename OutputT, typename LayoutC, typename LayoutD>
+    struct hipcc_bug_half_packing<
+        OutputT,
+        LayoutC,
+        LayoutD,
+        typename std::enable_if<sizeof(OutputT) == 2u && std::is_same<LayoutC, col_major>::value
+                                && std::is_same<LayoutD, col_major>::value>::type> : std::true_type
+    {
+    };
+
+} // namespace quirks
+
