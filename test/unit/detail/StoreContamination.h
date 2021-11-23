989a9fbbe4069585e92a27fb1cb7d775e227adb7
Chris Millette <chris.millette@amd.com>
2021-11-23 14:01:01 -0500

    Add Contamination test coverage for A, B, Acc fragments

--- test/unit/detail/StoreContamination.h
+++ test/unit/detail/StoreContamination.h
@@ -32,62 +32,118 @@
 
 // Wrapper into the actual device function
 template <uint32_t BlockM, uint32_t BlockN, typename DataT, typename Layout>
-struct StoreContaminationKernel final : public UnitKernelBase<BlockM, BlockN, DataT, Layout>
+struct StoreContaminationKernel : public UnitKernelBase<BlockM, BlockN, DataT, Layout>
 {
 private:
     using Base = UnitKernelBase<BlockM, BlockN, DataT, Layout>;
 
 public:
-    StoreContaminationKernel() {}
-    ~StoreContaminationKernel() final {}
+    StoreContaminationKernel()          = default;
+    virtual ~StoreContaminationKernel() = default;
 
-    void setupImpl(typename Base::DataStorage::ProblemSize const& probsize) final
+    void setupImpl(typename Base::DataStorage::ProblemSize const& probSize) final
     {
         auto& dataInstance = Base::DataStorage::instance();
 
-        Base::mParam1 = Base::mM + 2;
-        Base::mParam2 = Base::mN + 2;
+        // Make some rectangular padding around the area of interest
+        // Param1 = padM
+        // Param2 = padN
+        using SizeT  = typename Base::DataStorage::ProblemSize;
+        using IndexT = typename std::tuple_element<0, SizeT>::type;
+        auto paddedProbSize
+            = std::make_pair(std::get<0>(probSize) + 2 * static_cast<IndexT>(Base::mParam1),
+                             std::get<1>(probSize) + 2 * static_cast<IndexT>(Base::mParam2));
 
-        // Initialize matrix storage
-        const int64_t sizeD = Base::mM * Base::mN;
-        dataInstance->resizeStorage(probsize);
+        auto paddedElements = std::get<0>(paddedProbSize) * std::get<1>(paddedProbSize);
 
-        // Initialize matrix data on host
+        // Initialize matrix storage with padded size.
+        // Padded size >= MxN
+        dataInstance->resizeStorage(paddedProbSize);
+
+        // Initialize input data on host (not padded)
         MatrixUtil<Layout>::fill(dataInstance->hostIn().get(), Base::mM, Base::mN);
+        dataInstance->copyData(dataInstance->deviceIn(), dataInstance->hostIn(), paddedElements);
 
-        dataInstance->copyData(dataInstance->deviceIn(), dataInstance->hostIn(), sizeD);
+        // Initialize output data on host (padded size, all with marker elements)
+        auto outputInit = dataInstance->template allocHost<DataT>(paddedElements);
+        MatrixUtil<Layout>::fill(outputInit.get(),
+                                 std::get<0>(paddedProbSize),
+                                 std::get<1>(paddedProbSize),
+                                 std::numeric_limits<DataT>::max());
+        dataInstance->copyData(dataInstance->deviceOut(), outputInit, paddedElements);
     }
 
-    void validateResultsImpl()
+    void validateResultsImpl() final
     {
         auto& dataInstance = Base::DataStorage::instance();
+        auto& kernelResult = dataInstance->hostIn();
 
-        // Allocated managed memory for results on host
-        const int64_t sizeD        = Base::mM * Base::mN;
-        auto          kernelResult = dataInstance->template allocHost<DataT>(sizeD);
+        // Re-use host in memory for result
+        // Use padded MxN as output is padded
+        auto          paddedSize     = dataInstance->problemSize();
+        const int64_t paddedElements = std::get<0>(paddedSize) * std::get<1>(paddedSize);
 
         // Cache current kernel result from device
-        dataInstance->copyData(kernelResult, dataInstance->deviceOut(), sizeD);
+        dataInstance->copyData(kernelResult, dataInstance->deviceOut(), paddedElements);
 
-        double errorTolerance = 10.0;
+        double errorTolerance = 1.0;
 
-        std::tie(Base::mValidationResult, Base::mMaxRelativeError)
-            = compareEqual<DataT, DataT, Layout, Layout>(kernelResult.get(),
-                                                         dataInstance->hostIn().get(),
-                                                         Base::mM,
-                                                         Base::mN,
-                                                         errorTolerance);
+        // See if our output padding contains any contamination
+        auto result = countPaddingVal<DataT, Layout>(kernelResult.get(),
+                                                     Base::mM,
+                                                     Base::mN,
+                                                     static_cast<uint32_t>(Base::mParam1),
+                                                     static_cast<uint32_t>(Base::mParam2),
+                                                     std::numeric_limits<DataT>::max(),
+                                                     errorTolerance);
 
-        EXPECT_TRUE(Base::mValidationResult) << "Max relative error: " << Base::mMaxRelativeError;
+        // All padding must be markers
+        Base::mValidationResult = (result == (paddedElements - Base::mM * Base::mN));
     }
 
+    typename Base::KernelFunc kernelImpl() const = 0;
+};
+
+template <uint32_t BlockM, uint32_t BlockN, typename DataT, typename Layout>
+struct StoreContaminationKernelA : public StoreContaminationKernel<BlockM, BlockN, DataT, Layout>
+{
+private:
+    using Base = StoreContaminationKernel<BlockM, BlockN, DataT, Layout>;
+
+protected:
     typename Base::KernelFunc kernelImpl() const final
     {
-        return typename Base::KernelFunc(storeContamination<BlockM, BlockN, DataT, Layout>);
+        return typename Base::KernelFunc(storeContaminationA<BlockM, BlockN, DataT, Layout>);
     }
 };
 
-// This is the GeneratorImpl class
+template <uint32_t BlockM, uint32_t BlockN, typename DataT, typename Layout>
+struct StoreContaminationKernelB : public StoreContaminationKernel<BlockM, BlockN, DataT, Layout>
+{
+private:
+    using Base = StoreContaminationKernel<BlockM, BlockN, DataT, Layout>;
+
+protected:
+    typename Base::KernelFunc kernelImpl() const final
+    {
+        return typename Base::KernelFunc(storeContaminationB<BlockM, BlockN, DataT, Layout>);
+    }
+};
+
+template <uint32_t BlockM, uint32_t BlockN, typename DataT, typename Layout>
+struct StoreContaminationKernelAcc : public StoreContaminationKernel<BlockM, BlockN, DataT, Layout>
+{
+private:
+    using Base = StoreContaminationKernel<BlockM, BlockN, DataT, Layout>;
+
+protected:
+    typename Base::KernelFunc kernelImpl() const final
+    {
+        return typename Base::KernelFunc(storeContaminationAcc<BlockM, BlockN, DataT, Layout>);
+    }
+};
+
+template <template <uint32_t, uint32_t, typename, typename> class KernelClass>
 struct StoreContaminationGenerator
 {
     // Indices to test parameters
@@ -106,15 +162,18 @@ struct StoreContaminationGenerator
     {
         // Map GTest params to Kernel params
         using TestParamsT = std::tuple<Ts...>;
-        using KernelT
-            = StoreContaminationKernel<std::tuple_element_t<BlockM, TestParamsT>::value, // BlockM
-                                       std::tuple_element_t<BlockN, TestParamsT>::value, // BlockN
-                                       std::tuple_element_t<DataT, TestParamsT>, // DataT
-                                       std::tuple_element_t<Layout, TestParamsT> // Layout
-                                       >;
+        using KernelT     = KernelClass<std::tuple_element_t<BlockM, TestParamsT>::value, // BlockM
+                                    std::tuple_element_t<BlockN, TestParamsT>::value, // BlockN
+                                    std::tuple_element_t<DataT, TestParamsT>, // DataT
+                                    std::tuple_element_t<Layout, TestParamsT> // Layout
+                                    >;
 
         return std::make_shared<KernelT>();
     }
 };
+
+using StoreContaminationGeneratorA   = StoreContaminationGenerator<StoreContaminationKernelA>;
+using StoreContaminationGeneratorB   = StoreContaminationGenerator<StoreContaminationKernelB>;
+using StoreContaminationGeneratorAcc = StoreContaminationGenerator<StoreContaminationKernelAcc>;
 
 #endif // WMMA_DETAIL_STORE_CONTAMINATION_H
--- test/unit/device/StoreContamination.h
+++ test/unit/device/StoreContamination.h
@@ -31,21 +31,87 @@
 #include "WMMA.h"
 
 template <uint32_t BlockM, uint32_t BlockN, typename DataT, typename Layout>
-__global__ void storeContamination(
+__global__ void storeContaminationA(
     uint32_t m, uint32_t n, DataT const* in, DataT* out, uint32_t ld, DataT param1, DataT param2)
 {
     using Mapping = MappingUtil<BlockM, BlockN, DataT, Layout>;
 
-    int paddedld = std::is_same<Layout, row_major>::value ? param2 : param1;
+    // Mapping:
+    // Incoming -> Matrix A (ColNT)
+    // BlockM -> BlockM
+    // <Dummy> -> BlockN
+    // BlockN -> BlockK
+    auto frag = wmma::fragment<matrix_a, BlockM, 1, BlockN, DataT, Layout>();
 
-    // Create frags and fill
-    auto frag = wmma::fragment<accumulator, BlockM, BlockN, 1, DataT, Layout>();
-
+    // Output is padded.
+    // Make sure to offset write coords and extend writing ld.
+    uint32_t paddedLd
+        = ld + 2 * static_cast<uint32_t>(std::is_same<Layout, row_major>::value ? param2 : param1);
+    auto writeMatCoord = Mapping::matrixCoord();
+    auto writeMatCoordPadded
+        = std::make_pair(std::get<0>(writeMatCoord) + static_cast<uint32_t>(param1),
+                         std::get<1>(writeMatCoord) + static_cast<uint32_t>(param2));
     // Map, load and store.
     auto* read  = Mapping::dataCoord(in, ld);
-    auto* write = Mapping::dataCoord(out, ld);
+    auto* write = Mapping::dataCoord(out, paddedLd, writeMatCoordPadded);
     wmma::load_matrix_sync(frag, read, ld);
-    wmma::store_matrix_sync(write, frag, ld);
+    wmma::store_matrix_sync(write, frag, paddedLd);
+}
+
+template <uint32_t BlockM, uint32_t BlockN, typename DataT, typename Layout>
+__global__ void storeContaminationB(
+    uint32_t m, uint32_t n, DataT const* in, DataT* out, uint32_t ld, DataT param1, DataT param2)
+{
+    using Mapping = MappingUtil<BlockM, BlockN, DataT, Layout>;
+
+    // Mapping:
+    // Incoming -> Matrix B (RowNT)
+    // <Dummy> -> BlockM
+    // BlockN -> BlockN
+    // BlockM -> BlockK
+    auto frag = wmma::fragment<matrix_b, 1, BlockN, BlockM, DataT, Layout>();
+
+    // Output is padded.
+    // Make sure to offset write coords and extend writing ld.
+    uint32_t paddedLd
+        = ld + 2 * static_cast<uint32_t>(std::is_same<Layout, row_major>::value ? param2 : param1);
+    auto writeMatCoord = Mapping::matrixCoord();
+    auto writeMatCoordPadded
+        = std::make_pair(std::get<0>(writeMatCoord) + static_cast<uint32_t>(param1),
+                         std::get<1>(writeMatCoord) + static_cast<uint32_t>(param2));
+    // Map, load and store.
+    auto* read  = Mapping::dataCoord(in, ld);
+    auto* write = Mapping::dataCoord(out, paddedLd, writeMatCoordPadded);
+    wmma::load_matrix_sync(frag, read, ld);
+    wmma::store_matrix_sync(write, frag, paddedLd);
+}
+
+template <uint32_t BlockM, uint32_t BlockN, typename DataT, typename Layout>
+__global__ void storeContaminationAcc(
+    uint32_t m, uint32_t n, DataT const* in, DataT* out, uint32_t ld, DataT param1, DataT param2)
+{
+    using Mapping = MappingUtil<BlockM, BlockN, DataT, Layout>;
+
+    // Mapping:
+    // Incoming -> Matrix C (Row4T)
+    // BlockM -> BlockM
+    // BlockN -> BlockN
+    // <Dummy> -> BlockK
+    auto frag = wmma::fragment<accumulator, BlockM, BlockN, 1, DataT, Layout>();
+
+    // Output is padded.
+    // Make sure to offset write coords and extend writing ld.
+    uint32_t paddedLd
+        = ld + 2 * static_cast<uint32_t>(std::is_same<Layout, row_major>::value ? param2 : param1);
+    auto writeMatCoord = Mapping::matrixCoord();
+    auto writeMatCoordPadded
+        = std::make_pair(std::get<0>(writeMatCoord) + static_cast<uint32_t>(param1),
+                         std::get<1>(writeMatCoord) + static_cast<uint32_t>(param2));
+    // Map, load and store.
+    auto* read  = Mapping::dataCoord(in, ld);
+    auto* write = Mapping::dataCoord(out, paddedLd, writeMatCoordPadded);
+    wmma::load_matrix_sync(frag, read, ld);
+    wmma::store_matrix_sync(write, frag, paddedLd);
 }
 
 #endif // WMMA_DEVICE_STORE_CONTAMINATION_H
