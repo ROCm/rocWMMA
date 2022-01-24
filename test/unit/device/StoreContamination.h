6675381d7a69ae3d0a50c7764bb5d22487b2fc5c
Chris Millette <chris.millette@amd.com>
2022-01-24 17:53:09 -0500

    Update contamination device

--- test/unit/device/StoreContamination.h
+++ test/unit/device/StoreContamination.h
@@ -30,94 +30,115 @@
 #include "MappingUtil.h"
 #include "WMMA.h"
 
-template <uint32_t BlockM, uint32_t BlockN, typename DataT, typename Layout>
-__global__ void storeContaminationA(
-    uint32_t m, uint32_t n, DataT const* in, DataT* out, uint32_t ld, DataT param1, DataT param2)
+namespace rocwmma
 {
-    using Mapping = rocwmma::MappingUtil<BlockM, BlockN, DataT, Layout>;
 
-    // Mapping:
-    // Incoming -> Matrix A (ColNT)
-    // BlockM -> BlockM
-    // <Dummy> -> BlockN
-    // BlockN -> BlockK
-    auto frag = rocwmma::fragment<rocwmma::matrix_a, BlockM, 1, BlockN, DataT, Layout>();
+    template <uint32_t BlockM, uint32_t BlockN, typename DataT, typename Layout>
+    __global__ void storeContaminationA(uint32_t     m,
+                                        uint32_t     n,
+                                        DataT const* in,
+                                        DataT*       out,
+                                        uint32_t     ld,
+                                        DataT        param1,
+                                        DataT        param2)
+    {
+        using Mapping = rocwmma::MappingUtil<BlockM, BlockN, DataT, Layout>;
 
-    // Output is padded.
-    // Make sure to offset write coords and extend writing ld.
-    uint32_t paddedLd
-        = ld
-          + 2
-                * static_cast<uint32_t>(std::is_same<Layout, rocwmma::row_major>::value ? param2
-                                                                                        : param1);
-    auto writeMatCoord = Mapping::matrixCoord();
-    auto writeMatCoordPadded
-        = std::make_pair(std::get<0>(writeMatCoord) + static_cast<uint32_t>(param1),
-                         std::get<1>(writeMatCoord) + static_cast<uint32_t>(param2));
-    // Map, load and store.
-    auto* read  = Mapping::dataCoord(in, ld);
-    auto* write = Mapping::dataCoord(out, writeMatCoordPadded, paddedLd);
-    rocwmma::load_matrix_sync(frag, read, ld);
-    rocwmma::store_matrix_sync(write, frag, paddedLd);
-}
+        // Mapping:
+        // Incoming -> Matrix A (ColNT)
+        // BlockM -> BlockM
+        // <Dummy> -> BlockN
+        // BlockN -> BlockK
+        auto frag = rocwmma::fragment<rocwmma::matrix_a, BlockM, 1, BlockN, DataT, Layout>();
 
-template <uint32_t BlockM, uint32_t BlockN, typename DataT, typename Layout>
-__global__ void storeContaminationB(
-    uint32_t m, uint32_t n, DataT const* in, DataT* out, uint32_t ld, DataT param1, DataT param2)
-{
-    using Mapping = rocwmma::MappingUtil<BlockM, BlockN, DataT, Layout>;
+        // Output is padded.
+        // Make sure to offset write coords and extend writing ld.
+        uint32_t paddedLd
+            = ld
+              + 2
+                    * static_cast<uint32_t>(
+                        std::is_same<Layout, rocwmma::row_major>::value ? param2 : param1);
+        auto writeMatCoord = Mapping::matrixCoord();
+        auto writeMatCoordPadded
+            = std::make_pair(std::get<0>(writeMatCoord) + static_cast<uint32_t>(param1),
+                             std::get<1>(writeMatCoord) + static_cast<uint32_t>(param2));
+        // Map, load and store.
+        auto* read  = Mapping::dataCoord(in, ld);
+        auto* write = Mapping::dataCoord(out, writeMatCoordPadded, paddedLd);
+        rocwmma::load_matrix_sync(frag, read, ld);
+        rocwmma::store_matrix_sync(write, frag, paddedLd);
+    }
 
-    // Mapping:
-    // Incoming -> Matrix B (RowNT)
-    // <Dummy> -> BlockM
-    // BlockN -> BlockN
-    // BlockM -> BlockK
-    auto frag = rocwmma::fragment<rocwmma::matrix_b, 1, BlockN, BlockM, DataT, Layout>();
+    template <uint32_t BlockM, uint32_t BlockN, typename DataT, typename Layout>
+    __global__ void storeContaminationB(uint32_t     m,
+                                        uint32_t     n,
+                                        DataT const* in,
+                                        DataT*       out,
+                                        uint32_t     ld,
+                                        DataT        param1,
+                                        DataT        param2)
+    {
+        using Mapping = rocwmma::MappingUtil<BlockM, BlockN, DataT, Layout>;
 
-    // Output is padded.
-    // Make sure to offset write coords and extend writing ld.
-    uint32_t paddedLd
-        = ld
-          + 2
-                * static_cast<uint32_t>(std::is_same<Layout, rocwmma::row_major>::value ? param2
-                                                                                        : param1);
-    auto writeMatCoord = Mapping::matrixCoord();
-    auto writeMatCoordPadded
-        = std::make_pair(std::get<0>(writeMatCoord) + static_cast<uint32_t>(param1),
-                         std::get<1>(writeMatCoord) + static_cast<uint32_t>(param2));
-    // Map, load and store.
-    auto* read  = Mapping::dataCoord(in, ld);
-    auto* write = Mapping::dataCoord(out, writeMatCoordPadded, paddedLd);
-    rocwmma::load_matrix_sync(frag, read, ld);
-    rocwmma::store_matrix_sync(write, frag, paddedLd);
-}
+        // Mapping:
+        // Incoming -> Matrix B (RowNT)
+        // <Dummy> -> BlockM
+        // BlockN -> BlockN
+        // BlockM -> BlockK
+        auto frag = rocwmma::fragment<rocwmma::matrix_b, 1, BlockN, BlockM, DataT, Layout>();
 
-template <uint32_t BlockM, uint32_t BlockN, typename DataT, typename Layout>
-__global__ void storeContaminationAcc(
-    uint32_t m, uint32_t n, DataT const* in, DataT* out, uint32_t ld, DataT param1, DataT param2)
-{
-    using Mapping = rocwmma::MappingUtil<BlockM, BlockN, DataT, Layout>;
+        // Output is padded.
+        // Make sure to offset write coords and extend writing ld.
+        uint32_t paddedLd
+            = ld
+              + 2
+                    * static_cast<uint32_t>(
+                        std::is_same<Layout, rocwmma::row_major>::value ? param2 : param1);
+        auto writeMatCoord = Mapping::matrixCoord();
+        auto writeMatCoordPadded
+            = std::make_pair(std::get<0>(writeMatCoord) + static_cast<uint32_t>(param1),
+                             std::get<1>(writeMatCoord) + static_cast<uint32_t>(param2));
+        // Map, load and store.
+        auto* read  = Mapping::dataCoord(in, ld);
+        auto* write = Mapping::dataCoord(out, writeMatCoordPadded, paddedLd);
+        rocwmma::load_matrix_sync(frag, read, ld);
+        rocwmma::store_matrix_sync(write, frag, paddedLd);
+    }
 
-    // Mapping:
-    // Incoming -> Matrix C (Row4T)
-    // BlockM -> BlockM
-    // BlockN -> BlockN
-    // <Dummy> -> BlockK
-    auto frag = rocwmma::fragment<rocwmma::accumulator, BlockM, BlockN, 1, DataT, Layout>();
+    template <uint32_t BlockM, uint32_t BlockN, typename DataT, typename Layout>
+    __global__ void storeContaminationAcc(uint32_t     m,
+                                          uint32_t     n,
+                                          DataT const* in,
+                                          DataT*       out,
+                                          uint32_t     ld,
+                                          DataT        param1,
+                                          DataT        param2)
+    {
+        using Mapping = rocwmma::MappingUtil<BlockM, BlockN, DataT, Layout>;
 
-    // Output is padded.
-    // Make sure to offset write coords and extend writing ld.
-    uint32_t paddedLd
-        = ld + 2 * static_cast<uint32_t>(std::is_same<Layout, row_major>::value ? param2 : param1);
-    auto writeMatCoord = Mapping::matrixCoord();
-    auto writeMatCoordPadded
-        = std::make_pair(std::get<0>(writeMatCoord) + static_cast<uint32_t>(param1),
-                         std::get<1>(writeMatCoord) + static_cast<uint32_t>(param2));
-    // Map, load and store.
-    auto* read  = Mapping::dataCoord(in, ld);
-    auto* write = Mapping::dataCoord(out, writeMatCoordPadded, paddedLd);
-    rocwmma::load_matrix_sync(frag, read, ld);
-    rocwmma::store_matrix_sync(write, frag, paddedLd);
-}
+        // Mapping:
+        // Incoming -> Matrix C (Row4T)
+        // BlockM -> BlockM
+        // BlockN -> BlockN
+        // <Dummy> -> BlockK
+        auto frag = rocwmma::fragment<rocwmma::accumulator, BlockM, BlockN, 1, DataT, Layout>();
+
+        // Output is padded.
+        // Make sure to offset write coords and extend writing ld.
+        uint32_t paddedLd
+            = ld
+              + 2 * static_cast<uint32_t>(std::is_same<Layout, row_major>::value ? param2 : param1);
+        auto writeMatCoord = Mapping::matrixCoord();
+        auto writeMatCoordPadded
+            = std::make_pair(std::get<0>(writeMatCoord) + static_cast<uint32_t>(param1),
+                             std::get<1>(writeMatCoord) + static_cast<uint32_t>(param2));
+        // Map, load and store.
+        auto* read  = Mapping::dataCoord(in, ld);
+        auto* write = Mapping::dataCoord(out, writeMatCoordPadded, paddedLd);
+        rocwmma::load_matrix_sync(frag, read, ld);
+        rocwmma::store_matrix_sync(write, frag, paddedLd);
+    }
+
+} // namespace rocwmma
 
 #endif // WMMA_DEVICE_STORE_CONTAMINATION_H
