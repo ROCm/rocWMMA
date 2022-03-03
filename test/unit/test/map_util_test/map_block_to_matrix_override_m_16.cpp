a19444e16c3d1f8e20126e7d3341851ebace126c
Chris Millette <chris.millette@amd.com>
2022-03-03 12:04:31 -0500

    Add test coverage sizes for map_block_to_matrix_override
    [ROCm/rocwmma commit: 7284f8b9a769c6e8720dd19d0b47f587425d8bc0]

--- test/unit/test/map_util_test/map_block_to_matrix_override_m_16.cpp
+++ test/unit/test/map_util_test/map_block_to_matrix_override_m_16.cpp
@@ -38,16 +38,16 @@ namespace rocwmma
         using Base = UnitTestParams;
 
         // Types: ALL + double
-        // Block Sizes: 32 x 32 x BlockK
-        // Layouts: T
-        using Types        = typename Base::TestTypes32x32;
-        using BlockSizes   = typename Base::TestBlockSizes32x32;
-        using Layouts      = typename Base::TestLayoutsT;
+        // Block Sizes: 16 x BlockN
+        // Layouts: NT
+        using Types        = typename Base::TestTypes16x16;
+        using BlockSizes   = typename Base::TestBlockSizes16x16;
+        using Layouts      = typename Base::TestLayoutsAll;
         using KernelParams = typename CombineLists<Types, BlockSizes, Layouts>::Result;
 
         // Assemble the kernel generator
-        // Kernel: MapBlockToMatrixOverrideN
-        using GeneratorImpl   = MapBlockToMatrixOverrideNGenerator;
+        // Kernel: MapBlockToMatrixOverrideM
+        using GeneratorImpl   = MapBlockToMatrixOverrideMGenerator;
         using KernelGenerator = KernelGenerator<KernelParams, GeneratorImpl>;
 
         // Sanity check for kernel generator
@@ -60,21 +60,21 @@ namespace rocwmma
         }
     };
 
-} // nameapace rocwmma
+} // namespace rocwmma
 
 // Test suite for unique parameterization
-class MapBlockToMatrixOverrideNTest32x32T : public rocwmma::UnitTest
+class MapBlockToMatrixOverrideMTest16 : public rocwmma::UnitTest
 {
 };
 
-TEST_P(MapBlockToMatrixOverrideNTest32x32T, RunKernel)
+TEST_P(MapBlockToMatrixOverrideMTest16, RunKernel)
 {
     this->RunKernel();
 }
 
 INSTANTIATE_TEST_SUITE_P(
     KernelTests,
-    MapBlockToMatrixOverrideNTest32x32T,
+    MapBlockToMatrixOverrideMTest16,
     ::testing::Combine(::testing::ValuesIn(rocwmma::TestParams::kernels()),
                        ::testing::ValuesIn(rocwmma::TestParams::threadBlocks()),
                        ::testing::ValuesIn(rocwmma::TestParams::problemSizes()),
