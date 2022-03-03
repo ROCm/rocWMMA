33f89533ce0b53395daed395fc94accf4e11ea86
Chris Millette <chris.millette@amd.com>
2022-03-02 19:17:13 -0500

    Fix and consolidate mapping util override code
    [ROCm/rocwmma commit: fc50370f6c03a959ba178cd7953bdab273fec0bb]

--- test/unit/detail/map_block_to_matrix_override.hpp
+++ test/unit/detail/map_block_to_matrix_override.hpp
@@ -24,34 +24,57 @@
  *
  *******************************************************************************/
 
-#ifndef ROCWMMA_DETAIL_MAP_BLOCK_TO_MATRIX_OVERRIDE_M_HPP
-#define ROCWMMA_DETAIL_MAP_BLOCK_TO_MATRIX_OVERRIDE_M_HPP
+#ifndef ROCWMMA_DETAIL_MAP_BLOCK_TO_MATRIX_OVERRIDE_HPP
+#define ROCWMMA_DETAIL_MAP_BLOCK_TO_MATRIX_OVERRIDE_HPP
 
-#include "device/map_block_to_matrix_override_m.hpp"
+#include "device/map_block_to_matrix_override.hpp"
 #include "unit_kernel_base.hpp"
 
 namespace rocwmma
 {
 
-    // Wrapper into the actual device function
     template <uint32_t BlockM, uint32_t BlockN, typename DataT, typename Layout>
-    struct MapBlockToMatrixOverrideMKernel final
+    struct MapBlockToMatrixOverrideKernel
         : public UnitKernelBase<BlockM, BlockN, DataT, Layout>
     {
     private:
         using Base = UnitKernelBase<BlockM, BlockN, DataT, Layout>;
 
+    protected:
+        enum : int32_t
+        {
+            OVERRIDE_M,
+            OVERRIDE_N
+
+        } mOverride;
+
     public:
-        MapBlockToMatrixOverrideMKernel()        = default;
-        ~MapBlockToMatrixOverrideMKernel() final = default;
+        MapBlockToMatrixOverrideKernel(decltype(mOverride) overrideContext)
+        : Base(), mOverride(overrideContext){};
+
+        virtual ~MapBlockToMatrixOverrideKernel() = default;
 
         void setupImpl(typename Base::DataStorage::ProblemSize const& probsize) final
         {
             auto& dataInstance = Base::DataStorage::instance();
-
             srand((unsigned)time(0));
-            uint32_t mBlocks = this->gridDim().x;
-            Base::mParam1    = static_cast<DataT>(static_cast<float32_t>(rand() % mBlocks));
+
+            // Determine override param:
+            // Base::mParam1 is stored as DataT, so we must make sure we
+            // can store an accurate integer representation for override.
+            // must be between [0, min(maxInt, blocks - 1)] for block rw.
+            auto maxInt = maxExactInteger<DataT>();
+            auto blocks = mOverride == OVERRIDE_M ? 
+                Base::gridDim().x * (Base::blockDim().x / AMDGCN_WAVE_SIZE) :
+                Base::gridDim().y * Base::blockDim().y;
+            using MaxIntT = decltype(maxInt);
+            auto upperBound = std::max(
+                static_cast<MaxIntT>(0), 
+                std::min(maxInt, static_cast<MaxIntT>(blocks - 1)));
+
+            // Modulate if non zero
+            upperBound = upperBound ? rand() % upperBound : upperBound;
+            Base::mParam1    = static_cast<DataT>(static_cast<float32_t>(upperBound));
 
             // Initialize matrix storage
             const int64_t sizeD = Base::mM * Base::mN;
@@ -70,49 +93,85 @@ namespace rocwmma
         {
             auto& dataInstance = Base::DataStorage::instance();
 
-            // Allocated managed memory for results on host
             const int64_t sizeD = Base::mM * Base::mN;
 
+            // Allocate and cache host memory for device result
             auto kernelResult = dataInstance->template allocHost<DataT>(sizeD);
-            auto hostResult   = dataInstance->hostIn().get();
-
-            //Allocate additional resource to validate only the overriden row of size Base::mN
-            auto kernelResultToValidate = dataInstance->template allocHost<DataT>(Base::mN);
-            auto hostResultToValidate   = dataInstance->template allocHost<DataT>(Base::mN);
-
-            // Cache current kernel result from device
             dataInstance->copyData(kernelResult, dataInstance->deviceOut(), sizeD);
 
-            double errorTolerance = 1.0;
-            // Validation offset starts at row Base::mParam1 * BlockM
-            // To get to row Base::mParam1 * BlockM,
-            // in case of row-major layout, skip Base::mParam1 * BlockM * Base::mN elements
-            // in case of col-major layout, skip only Base::mParam1 * BlockM elements
-            uint32_t baseOffset
-                = std::is_same<Layout, row_major>::value
-                      ? static_cast<uint32_t>(static_cast<float32_t>(Base::mParam1)) * BlockM
-                            * Base::mN
-                      : static_cast<uint32_t>(static_cast<float32_t>(Base::mParam1)) * BlockM;
+            // Allocate host memory for reference calcs
+            auto& hostInput   = dataInstance->hostIn();
+            auto hostResult   = dataInstance->template allocHost<DataT>(sizeD);
+                        
+            /// Initialize reference for validation
 
-            // To get to the elements across row,
-            // in case of row-major, next element access is current_element + 1
-            // in case of col-major, next element access is by current_element + Base::mN
-            uint32_t ld = std::is_same<Layout, row_major>::value ? 1 : Base::mM;
+            // Setup addressing for DataLayout
+            auto rowMjrOffset = [](std::pair<uint32_t, uint32_t> const& matrixCoord, uint32_t ld) 
+            { 
+                return std::get<0>(matrixCoord) * ld + std::get<1>(matrixCoord); 
+            };
+            auto colMjrOffset = [](std::pair<uint32_t, uint32_t> const& matrixCoord, uint32_t ld) 
+            { 
+                return std::get<1>(matrixCoord) * ld + std::get<0>(matrixCoord); 
+            };
+            auto ld = std::is_same<Layout, row_major>::value ? Base::mN : Base::mM;
+            auto arrayOffset = std::is_same<Layout, row_major>::value ? rowMjrOffset : colMjrOffset;
 
-            // Copy the entire row Base::mParam1 * BlockM
-            for(int i = 0; i < Base::mN; i++)
+            // Scale from workgroup grid to wave grid
+            auto waveGridDim = std::make_pair(Base::gridDim().x * (Base::blockDim().x / AMDGCN_WAVE_SIZE),
+                                              Base::gridDim().y * Base::blockDim().y);
+
+            // Calculate the expected output from the inputs
+            #pragma omp parallel for
+            for(auto row = 0u; row < std::get<0>(waveGridDim); row++)
             {
-                kernelResultToValidate[i] = kernelResult[baseOffset + (i * ld)];
-                hostResultToValidate[i]   = hostResult[baseOffset + (i * ld)];
-            }
+                #pragma omp parallel for
+                for(auto col = 0u; col < std::get<1>(waveGridDim); col++)
+                {
+                    // Setup read / write matrix coords for block
+                    auto overrideVal = static_cast<uint32_t>(static_cast<float32_t>(Base::mParam1));
+                    auto readBase  = std::make_pair((mOverride == OVERRIDE_M ? overrideVal : row) * BlockM,
+                                                    (mOverride == OVERRIDE_N ? overrideVal : col) * BlockN);
 
+                    auto writeBase = std::make_pair(row * BlockM,
+                                                    col * BlockN);
+
+                    // Loop through entire read block and copy to dest block
+                    for(auto rowOffset = 0u; rowOffset < BlockM; rowOffset++)
+                    {
+                        for(auto colOffset = 0u; colOffset < BlockN; colOffset++)
+                        {
+                            auto matrixRead = readBase + std::make_pair(rowOffset, colOffset);
+                            auto matrixWrite = writeBase + std::make_pair(rowOffset, colOffset);
+
+                            *(hostResult.get() + arrayOffset(matrixWrite, ld)) = 
+                                *(hostInput.get() + arrayOffset(matrixRead, ld));
+
+                        }
+                    }
+                }
+            }
+            
+            double errorTolerance = 1.0;
             std::tie(Base::mValidationResult, Base::mMaxRelativeError)
-                = compareEqual<DataT, DataT, Layout, Layout>(kernelResultToValidate.get(),
-                                                             hostResultToValidate.get(),
-                                                             1,
+                = compareEqual<DataT, DataT, Layout, Layout>(kernelResult.get(),
+                                                             hostResult.get(),
+                                                             Base::mM,
                                                              Base::mN,
                                                              errorTolerance);
         }
+    };
+
+    template <uint32_t BlockM, uint32_t BlockN, typename DataT, typename Layout>
+    struct MapBlockToMatrixOverrideMKernel final
+        : public MapBlockToMatrixOverrideKernel<BlockM, BlockN, DataT, Layout>
+    {
+    private:
+        using Base = MapBlockToMatrixOverrideKernel<BlockM, BlockN, DataT, Layout>;
+
+    public:
+        MapBlockToMatrixOverrideMKernel() : Base(Base::OVERRIDE_M) {};
+        ~MapBlockToMatrixOverrideMKernel() final = default;
 
         typename Base::KernelFunc kernelImpl() const final
         {
@@ -121,8 +180,29 @@ namespace rocwmma
         }
     };
 
+    template <uint32_t BlockM, uint32_t BlockN, typename DataT, typename Layout>
+    struct MapBlockToMatrixOverrideNKernel final
+        : public MapBlockToMatrixOverrideKernel<BlockM, BlockN, DataT, Layout>
+    {
+    private:
+        using Base = MapBlockToMatrixOverrideKernel<BlockM, BlockN, DataT, Layout>;
+
+    public:
+        MapBlockToMatrixOverrideNKernel() : Base(Base::OVERRIDE_N) {};
+        ~MapBlockToMatrixOverrideNKernel() final = default;
+
+        typename Base::KernelFunc kernelImpl() const final
+        {
+            return
+                typename Base::KernelFunc(MapBlockToMatrixOverrideN<BlockM, BlockN, DataT, Layout>);
+        }
+    };
+
     // This is the GeneratorImpl class
-    struct MapBlockToMatrixOverrideMGenerator
+    template<
+     template <uint32_t BlockM, uint32_t BlockN, typename DataT, typename Layout>
+     class OverrideClass>
+    struct MapBlockToMatrixOverrideGenerator
     {
         // Indices to test parameters
         enum : uint32_t
@@ -140,7 +220,7 @@ namespace rocwmma
         {
             // Map GTest params to Kernel params
             using TestParamsT = std::tuple<Ts...>;
-            using KernelT     = MapBlockToMatrixOverrideMKernel<
+            using KernelT     = OverrideClass<
                 std::tuple_element_t<BlockM, TestParamsT>::value, // BlockM
                 std::tuple_element_t<BlockN, TestParamsT>::value, // BlockN
                 std::tuple_element_t<DataT, TestParamsT>, // DataT
@@ -151,6 +231,9 @@ namespace rocwmma
         }
     };
 
+    using MapBlockToMatrixOverrideMGenerator = MapBlockToMatrixOverrideGenerator<MapBlockToMatrixOverrideMKernel>;
+    using MapBlockToMatrixOverrideNGenerator = MapBlockToMatrixOverrideGenerator<MapBlockToMatrixOverrideNKernel>;
+
 } // namespace rocwmma
 
-#endif // ROCWMMA_DETAIL_MAP_BLOCK_TO_MATRIX_OVERRIDE_M_HPP
+#endif // ROCWMMA_DETAIL_MAP_BLOCK_TO_MATRIX_OVERRIDE_HPP
