552ea29f0c2d8462972c01f436dc0c1b27f4cd59
Chris Millette <chris.millette@amd.com>
2022-05-10 10:27:23 -0400

    Adjust MaxVW cap to dwordx4
    [ROCm/rocwmma commit: f088c641b7be4f2957df5d2abf71b91ee3012d79]

--- library/include/rocwmma/internal/io_traits.hpp
+++ library/include/rocwmma/internal/io_traits.hpp
@@ -154,18 +154,19 @@ namespace rocwmma
         };
 
         /*
-* The following class is intended to provide optimistic suggestions for
-* IO vector widths. Given a certain block size, search for largest
-* vector width that could potentially be used during IO.
-*
-* Start testing at a default width of BlockK. Keep halving the vector width
-* until it can fit the entire block, or split evenly amongst IO iterations.
-*/
+        * The following class is intended to provide optimistic suggestions for
+        * IO vector widths. Given a certain block size, search for largest
+        * vector width that could potentially be used during IO, up to a maximum of
+        * dwordx4.
+        *
+        * Start testing at a default width of BlockK. Keep halving the vector width
+        * until it can fit the entire block, or split evenly amongst IO iterations.
+        */
 
         template <uint32_t BlockDim,
                   uint32_t BlockK,
                   typename DataT,
-                  uint32_t TestWidth = AMDGCN_CACHE_LINE_SIZE_BYTES / sizeof(DataT)>
+                  uint32_t TestWidth = AMDGCN_DWORD_SIZE_BYTES * 4 / sizeof(DataT)>
         struct VecWidthTraits
         {
             enum : uint32_t
