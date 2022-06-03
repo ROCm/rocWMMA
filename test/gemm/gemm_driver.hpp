caa740c317e5704f7e07c8d1e9b6ef68d0f908c8
Chris Millette <chris.millette@amd.com>
2022-06-03 17:46:24 -0400

    Update the GemmDriver to handle either 1d or 2d fragment buffers
    [ROCm/rocwmma commit: 07f3fdd0b2631db8f9c3fb7743b467b7d2f735ef]

--- test/gemm/gemm_driver.hpp
+++ test/gemm/gemm_driver.hpp
@@ -54,30 +54,24 @@ namespace rocwmma
             using LRFragB = typename LdsMapping::LRFragB;
 
             template <typename FragT>
-            using IOTraits = typename FragT::IOConfig::IOTraits;
-
-            template <typename FragT>
-            using DataT = typename FragT::element_type;
-
-            template <typename FragT>
             using MappingUtil = typename FragT::IOConfig::MappingUtil;
 
             // Ensure that splitCounts are the same on both sides of
             // global fetch and local writes to match fragment data locality.
-            constexpr static auto splitCountA = std::min((uint32_t)IOTraits<GRFragA>::IOCount,
-                                                         (uint32_t)IOTraits<LWFragA>::IOCount);
+            constexpr static auto splitCountA = std::min((uint32_t)GetIOTraits_t<GRFragA>::IOCount,
+                                                         (uint32_t)GetIOTraits_t<LWFragA>::IOCount);
 
-            constexpr static auto splitCountB = std::min((uint32_t)IOTraits<GRFragB>::IOCount,
-                                                         (uint32_t)IOTraits<LWFragB>::IOCount);
+            constexpr static auto splitCountB = std::min((uint32_t)GetIOTraits_t<GRFragB>::IOCount,
+                                                         (uint32_t)GetIOTraits_t<LWFragB>::IOCount);
 
             static_assert(
-                ((uint32_t)IOTraits<GRFragA>::IOCount % splitCountA == 0u)
-                    && ((uint32_t)IOTraits<LWFragA>::IOCount % splitCountA == 0u),
+                ((uint32_t)GetIOTraits_t<GRFragA>::IOCount % splitCountA == 0u)
+                    && ((uint32_t)GetIOTraits_t<LWFragA>::IOCount % splitCountA == 0u),
                 "splitCount A is not common divisor of GlobalRead and LocalWrite IOCounts");
 
             static_assert(
-                ((uint32_t)IOTraits<GRFragB>::IOCount % splitCountB == 0u)
-                    && ((uint32_t)IOTraits<LWFragB>::IOCount % splitCountB == 0u),
+                ((uint32_t)GetIOTraits_t<GRFragB>::IOCount % splitCountB == 0u)
+                    && ((uint32_t)GetIOTraits_t<LWFragB>::IOCount % splitCountB == 0u),
                 "splitCount B is not common divisor of GlobalRead and LocalWrite IOCounts");
 
             ///
@@ -87,66 +81,91 @@ namespace rocwmma
             // Broadcast value to fragment
             // Single or BlocksX * BlocksY frags
             template <typename FragT>
-            __device__ static inline void fill(FragT& frag, DataT<FragT> value);
+            __device__ static inline void fill(FragT& frag, GetDataType_t<FragT> value);
             template <typename FragT, uint32_t BlocksX, uint32_t BlocksY>
             __device__ static inline void fill(FragT (&frags)[BlocksX][BlocksY],
-                                               DataT<FragT> value);
+                                               GetDataType_t<FragT> value);
 
             ///
             /// Global R/W
             ///
 
             // Global A/B reads in cooperative mode
-            __device__ static inline void
-                globalReadCoopA(GRFragA& grFragA, DataT<GRFragA> const* gAddrA, uint32_t lda);
-            __device__ static inline void
-                globalReadCoopB(GRFragB& grFragB, DataT<GRFragB> const* gAddrB, uint32_t ldb);
+            template <uint32_t BlocksX>
+            __device__ static inline void globalReadCoopA(GRFragA (&fragsA)[BlocksX],
+                                                          GetDataType_t<GRFragA> const* gAddrA,
+                                                          uint32_t                      lda);
+            __device__ static inline void globalReadCoopA(GRFragA&                      grFragA,
+                                                          GetDataType_t<GRFragA> const* gAddrA,
+                                                          uint32_t                      lda);
+
+            template <uint32_t BlocksY>
+            __device__ static inline void globalReadCoopB(GRFragB (&fragsB)[BlocksY],
+                                                          GetDataType_t<GRFragB> const* gAddrB,
+                                                          uint32_t                      ldb);
+            __device__ static inline void globalReadCoopB(GRFragB&                      grFragB,
+                                                          GetDataType_t<GRFragB> const* gAddrB,
+                                                          uint32_t                      ldb);
 
             // Global C reads non-cooperative
             // Single or BlocksX * BlocksY frags
             template <uint32_t BlocksX, uint32_t BlocksY>
             __device__ static inline void globalReadC(MfmaFragC (&fragC)[BlocksX][BlocksY],
-                                                      DataT<MfmaFragC> const* gAddrC,
-                                                      uint32_t                ldc);
+                                                      GetDataType_t<MfmaFragC> const* gAddrC,
+                                                      uint32_t                        ldc);
             __device__ static inline void
-                globalReadC(MfmaFragC& fragC, DataT<MfmaFragC> const* gAddrC, uint32_t ldc);
+                globalReadC(MfmaFragC& fragC, GetDataType_t<MfmaFragC> const* gAddrC, uint32_t ldc);
 
             // Global D writes non-cooperative
             // Single or BlocksX * BlocksY frags
             template <uint32_t BlocksX, uint32_t BlocksY>
-            __device__ static inline void globalWriteD(DataT<MfmaFragD>* gAddrD,
+            __device__ static inline void globalWriteD(GetDataType_t<MfmaFragD>* gAddrD,
                                                        MfmaFragD const (&fragsD)[BlocksX][BlocksY],
                                                        uint32_t ldd);
-            __device__ static inline void
-                globalWriteD(DataT<MfmaFragD>* gAddrD, MfmaFragD const& fragD, uint32_t ldd);
+            __device__ static inline void globalWriteD(GetDataType_t<MfmaFragD>* gAddrD,
+                                                       MfmaFragD const&          fragD,
+                                                       uint32_t                  ldd);
 
             ///
             /// Local R/W
             ///
 
             // Local A/B writes in cooperative mode
-            __device__ static inline void
-                localWriteCoopA(DataT<GRFragA>* ldsAddr, GRFragA const& grFragA, uint32_t ldlds);
-            __device__ static inline void
-                localWriteCoopB(DataT<GRFragB>* ldsAddr, GRFragB const& grFragB, uint32_t ldlds);
+            template <uint32_t BlocksX>
+            __device__ static inline void localWriteCoopA(GetDataType_t<GRFragA>* ldsAddr,
+                                                          GRFragA const (&grFragsA)[BlocksX],
+                                                          uint32_t ldlds);
+            __device__ static inline void localWriteCoopA(GetDataType_t<GRFragA>* ldsAddr,
+                                                          GRFragA const&          grFragA,
+                                                          uint32_t                ldlds);
+
+            template <uint32_t BlocksY>
+            __device__ static inline void localWriteCoopB(GetDataType_t<GRFragB>* ldsAddr,
+                                                          GRFragB const (&grFragsB)[BlocksY],
+                                                          uint32_t ldlds);
+            __device__ static inline void localWriteCoopB(GetDataType_t<GRFragB>* ldsAddr,
+                                                          GRFragB const&          grFragB,
+                                                          uint32_t                ldlds);
 
             // Local A read non-cooperative
             // Single or BlocksX frags
             template <uint32_t BlocksX>
             __device__ static inline void localReadA(MfmaFragA (&fragsA)[BlocksX],
-                                                     DataT<MfmaFragA> const* ldsAddrA,
-                                                     uint32_t                ldlds);
-            __device__ static inline void
-                localReadA(MfmaFragA& fragsA, DataT<MfmaFragA> const* ldsAddrA, uint32_t ldlds);
+                                                     GetDataType_t<MfmaFragA> const* ldsAddrA,
+                                                     uint32_t                        ldlds);
+            __device__ static inline void localReadA(MfmaFragA&                      fragsA,
+                                                     GetDataType_t<MfmaFragA> const* ldsAddrA,
+                                                     uint32_t                        ldlds);
 
             // Local B read non-cooperative
             // Single or BlocksY frags
             template <uint32_t BlocksY>
             __device__ static inline void localReadB(MfmaFragB (&fragsB)[BlocksY],
-                                                     DataT<MfmaFragB> const* ldsAddrB,
-                                                     uint32_t                ldlds);
-            __device__ static inline void
-                localReadB(MfmaFragB& fragsB, DataT<MfmaFragB> const* ldsAddrB, uint32_t ldlds);
+                                                     GetDataType_t<MfmaFragB> const* ldsAddrB,
+                                                     uint32_t                        ldlds);
+            __device__ static inline void localReadB(MfmaFragB&                      fragsB,
+                                                     GetDataType_t<MfmaFragB> const* ldsAddrB,
+                                                     uint32_t                        ldlds);
 
             ///
             /// MFMA
@@ -172,15 +191,15 @@ namespace rocwmma
             template <uint32_t BlocksX, uint32_t BlocksY>
             __device__ static inline void
                                           uniformFma(MfmaFragD (&fragsD)[BlocksX][BlocksY],
-                                                     DataT<MfmaFragAcc> alpha,
+                                                     GetDataType_t<MfmaFragAcc> alpha,
                                                      MfmaFragAcc const (&fragsAcc)[BlocksX][BlocksY],
-                                                     DataT<MfmaFragAcc> beta,
+                                                     GetDataType_t<MfmaFragAcc> beta,
                                                      MfmaFragC const (&fragsC)[BlocksX][BlocksY]);
-            __device__ static inline void uniformFma(MfmaFragD&         fragD,
-                                                     DataT<MfmaFragAcc> alpha,
-                                                     MfmaFragAcc const& fragAcc,
-                                                     DataT<MfmaFragAcc> beta,
-                                                     MfmaFragC const&   fragC);
+            __device__ static inline void uniformFma(MfmaFragD&                 fragD,
+                                                     GetDataType_t<MfmaFragAcc> alpha,
+                                                     MfmaFragAcc const&         fragAcc,
+                                                     GetDataType_t<MfmaFragAcc> beta,
+                                                     MfmaFragC const&           fragC);
 
             ///
             /// Wave synchronization
