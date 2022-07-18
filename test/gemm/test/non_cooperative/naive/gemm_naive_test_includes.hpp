5b631d52e1ad03a3aee646aba3901c090916c495
Chris Millette <chris.millette@amd.com>
2022-07-18 19:09:51 -0400

    Move mma_sync_test into new folder structure

---
+++ test/gemm/test/non_cooperative/naive/gemm_naive_test_includes.hpp
@@ -0,0 +1,11 @@
+#ifndef ROCWMMA_GEMM_NAIVE_TEST_INCLUDES_HPP
+#define ROCWMMA_GEMM_NAIVE_TEST_INCLUDES_HPP
+
+// Common includes for all tests
+#include "detail/mma_sync.hpp"
+#include "test/common_test_params.hpp"
+#include "test/gemm_test.hpp"
+
+#include "kernel_generator.hpp"
+
+#endif // ROCWMMA_GEMM_NAIVE_TEST_INCLUDES_HPP
