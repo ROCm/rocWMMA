2338c82356c6661ca7355eb5df9c8db6c4bf339f
Chris Millette <chris.millette@amd.com>
2022-08-04 12:09:15 -0400

    Fix non-uniform branch bug
    [ROCm/rocwmma commit: 6892ef99aa24d10b4c3c9c8d65858c16754fe4e0]

--- test/gemm/gemm_PGR0_LB0_MP0_MB_NC/device/kernel_device_func.hpp
+++ test/gemm/gemm_PGR0_LB0_MP0_MB_NC/device/kernel_device_func.hpp
@@ -40,6 +40,18 @@
 
 namespace rocwmma
 {
+    ///
+    /// This class of kernel is an extension of the naive kernel whereas
+    /// each wave is responsible for calculating a macro tile area of
+    /// BlocksX * BlockM x BlocksY * BlockN
+    ///
+    /// Kernel behaviour is described by:
+    /// PGR0 = Prefetch Global Read = 0, no prefetch
+    /// LB0 = Lds Blocks = 0, no Lds usage
+    /// MP0 = Mfma Priority = 0, no setprio
+    /// MB = Multi-block, BlocksX * BlocksY > 1
+    /// NC = Non-cooperative
+    ///
     template <uint32_t BlockM,
               uint32_t BlockN,
               uint32_t BlockK,
@@ -83,151 +95,140 @@ namespace rocwmma
         std::get<0>(matrixCoordC) *= BlocksX;
         std::get<1>(matrixCoordC) *= BlocksY;
 
-        if((std::get<0>(matrixCoordC) + BlocksX * BlockM) <= m
-           && (std::get<1>(matrixCoordC) + BlocksY * BlockN) <= n && (BlockK < k))
+        if(std::get<0>(matrixCoordC) + BlocksX * BlockM > m
+           || std::get<1>(matrixCoordC) + BlocksY * BlockN > n)
         {
-            // Targets for C sub-matrices
-            CoordT  subMatrixCoordsC[BlocksX][BlocksY];
-            FragAcc fragsAccum[BlocksX][BlocksY];
+            return;
+        }
 
-            /// Initialization
+        if(BlockK > k)
+        {
+            return;
+        }
+
+        // Targets for C sub-matrices
+        CoordT  subMatrixCoordsC[BlocksX][BlocksY];
+        FragAcc fragsAccum[BlocksX][BlocksY];
+
+        /// Initialization
 #pragma unroll
+        for(int i = 0; i < BlocksX; i++)
+        {
+#pragma unroll
+            for(int j = 0; j < BlocksY; j++)
+            {
+                // Initialize sub matrix coordinates
+                std::get<0>(subMatrixCoordsC[i][j]) += std::get<0>(matrixCoordC) + i * BlockM;
+                std::get<1>(subMatrixCoordsC[i][j]) += std::get<1>(matrixCoordC) + j * BlockN;
+
+                // Initialize accumulators
+                fill_fragment(fragsAccum[i][j], static_cast<ComputeT>(0));
+            }
+        }
+
+        /// Setup global read addresses
+        InputT const* globalAddrsA[BlocksX];
+        InputT const* globalAddrsB[BlocksY];
+
+        // Blocks in the same row share the same starting address for A
+#pragma unroll
+        for(int i = 0; i < BlocksX; i++)
+        {
+            globalAddrsA[i] = MappingA::dataCoord(
+                a, std::make_pair(std::get<0>(subMatrixCoordsC[i][0]), 0), lda);
+        }
+
+        // Blocks in the same col share the same starting address for B
+#pragma unroll
+        for(int i = 0; i < BlocksY; i++)
+        {
+            globalAddrsB[i] = MappingB::dataCoord(
+                b, std::make_pair(0, std::get<1>(subMatrixCoordsC[0][i])), ldb);
+        }
+
+        /// Setup address increments.
+        // A steps BlockK through m x k
+        // B steps BlockK through k x n
+        auto incrA  = MappingA::dataOffset(std::make_pair(0, BlockK), lda);
+        auto incrB  = MappingB::dataOffset(std::make_pair(BlockK, 0), ldb);
+        auto stepsK = k / BlockK;
+
+        /// Accumulate A * B
+        for(int currentStep = 0; currentStep < stepsK; currentStep++)
+        {
+            // Cache the B-Blocks for re-use
+            FragB cachedFragsB[BlocksY];
+            FragA fragA;
+
+            // Synchronize workgroup increases chances for cache hits
+            synchronize_workgroup();
+
+#pragma unroll
+            for(int j = 0; j < BlocksY; j++)
+            {
+                load_matrix_sync(cachedFragsB[j], globalAddrsB[j], ldb);
+                globalAddrsB[j] += incrB;
+            }
+
+            //#pragma unroll
             for(int i = 0; i < BlocksX; i++)
             {
-#pragma unroll
+                // A fragment will be re-used for each B
+                load_matrix_sync(fragA, globalAddrsA[i], lda);
+                globalAddrsA[i] += incrA;
+
+                //#pragma unroll
                 for(int j = 0; j < BlocksY; j++)
                 {
-                    // Initialize sub matrix coordinates
-                    std::get<0>(subMatrixCoordsC[i][j]) += std::get<0>(matrixCoordC) + i * BlockM;
-                    std::get<1>(subMatrixCoordsC[i][j]) += std::get<1>(matrixCoordC) + j * BlockN;
-
-                    // Initialize accumulators
-                    fill_fragment(fragsAccum[i][j], static_cast<ComputeT>(0));
+                    mma_sync(fragsAccum[i][j], fragA, cachedFragsB[j], fragsAccum[i][j]);
                 }
             }
+        }
 
-            /// Accumulate A * B
-            if(alpha)
+        // Initialize C frags
+        FragC fragsC[BlocksX][BlocksY];
+
+#pragma unroll
+        for(int i = 0; i < BlocksX; i++)
+        {
+#pragma unroll
+            for(int j = 0; j < BlocksY; j++)
             {
-                // Setup global read addresses
-                InputT const* globalAddrsA[BlocksX];
-                InputT const* globalAddrsB[BlocksY];
-
-                // Blocks in the same row share the same starting address for A
-#pragma unroll
-                for(int i = 0; i < BlocksX; i++)
-                {
-                    globalAddrsA[i] = MappingA::dataCoord(
-                        a, std::make_pair(std::get<0>(subMatrixCoordsC[i][0]), 0), lda);
-                }
-
-                // Blocks in the same col share the same starting address for B
-#pragma unroll
-                for(int i = 0; i < BlocksY; i++)
-                {
-                    globalAddrsB[i] = MappingB::dataCoord(
-                        b, std::make_pair(0, std::get<1>(subMatrixCoordsC[0][i])), ldb);
-                }
-
-                // Setup address increments.
-                // A steps BlockK through m x k
-                // B steps BlockK through k x n
-                auto incrA  = MappingA::dataOffset(std::make_pair(0, BlockK), lda);
-                auto incrB  = MappingB::dataOffset(std::make_pair(BlockK, 0), ldb);
-                auto stepsK = k / BlockK;
-
-                for(int currentStep = 0; currentStep < stepsK; currentStep++)
-                {
-                    // Cache the B-Blocks for re-use
-                    FragB cachedFragsB[BlocksY];
-                    FragA fragA;
-
-                    // Synchronize workgroup increases chances for cache hits
-                    synchronize_workgroup();
-
-#pragma unroll
-                    for(int j = 0; j < BlocksY; j++)
-                    {
-                        load_matrix_sync(cachedFragsB[j], globalAddrsB[j], ldb);
-                        globalAddrsB[j] += incrB;
-                    }
-
-                    //#pragma unroll
-                    for(int i = 0; i < BlocksX; i++)
-                    {
-                        // A fragment will be re-used for each B
-                        load_matrix_sync(fragA, globalAddrsA[i], lda);
-                        globalAddrsA[i] += incrA;
-
-                        //#pragma unroll
-                        for(int j = 0; j < BlocksY; j++)
-                        {
-                            mma_sync(fragsAccum[i][j], fragA, cachedFragsB[j], fragsAccum[i][j]);
-                        }
-                    }
-                }
+                auto* addrC = MappingC::dataCoord(c, subMatrixCoordsC[i][j], ldc);
+                load_matrix_sync(fragsC[i][j],
+                                 addrC,
+                                 ldc,
+                                 std::is_same<LayoutC, row_major>::value ? mem_row_major
+                                                                         : mem_col_major);
             }
+        }
 
-            // Initialize C frags
-            FragC fragsC[BlocksX][BlocksY];
-
-            if(beta)
+        // D = alpha * accumAB + beta * C
+#pragma unroll
+        for(int i = 0; i < BlocksX; i++)
+        {
+#pragma unroll
+            for(int j = 0; j < BlocksY; j++)
             {
+                auto& fragAcc = fragsAccum[i][j];
+                auto& fragC   = fragsC[i][j];
+
 #pragma unroll
-                for(int i = 0; i < BlocksX; i++)
+                for(int e = 0; e < fragC.num_elements; ++e)
                 {
-#pragma unroll
-                    for(int j = 0; j < BlocksY; j++)
-                    {
-                        auto* addrC = MappingC::dataCoord(c, subMatrixCoordsC[i][j], ldc);
-                        load_matrix_sync(fragsC[i][j],
-                                         addrC,
-                                         ldc,
-                                         std::is_same<LayoutC, row_major>::value ? mem_row_major
-                                                                                 : mem_col_major);
-                    }
+                    fragC.x[e]
+                        = OutputT(alpha * ComputeT(fragAcc.x[e]) + beta * ComputeT(fragC.x[e]));
                 }
-            }
-            else
-            {
-#pragma unroll
-                for(int i = 0; i < BlocksX; i++)
-                {
-#pragma unroll
-                    for(int j = 0; j < BlocksY; j++)
-                    {
-                        fill_fragment(fragsC[i][j], static_cast<OutputT>(0));
-                    }
-                }
-            }
 
-// D = alpha * accumAB + beta * C
-#pragma unroll
-            for(int i = 0; i < BlocksX; i++)
-            {
-#pragma unroll
-                for(int j = 0; j < BlocksY; j++)
-                {
-                    auto& fragAcc = fragsAccum[i][j];
-                    auto& fragC   = fragsC[i][j];
+                // Output addresss
+                auto* addrD = MappingD::dataCoord(d, subMatrixCoordsC[i][j], ldd);
 
-#pragma unroll
-                    for(int i = 0; i < fragC.num_elements; ++i)
-                    {
-                        fragC.x[i]
-                            = OutputT(alpha * ComputeT(fragAcc.x[i]) + beta * ComputeT(fragC.x[i]));
-                    }
-
-                    // Output addresss
-                    auto* addrD = MappingD::dataCoord(d, subMatrixCoordsC[i][j], ldd);
-
-                    // Store the output
-                    store_matrix_sync(addrD,
-                                      fragC,
-                                      ldd,
-                                      std::is_same<LayoutD, row_major>::value ? mem_row_major
-                                                                              : mem_col_major);
-                }
+                // Store the output
+                store_matrix_sync(addrD,
+                                  fragC,
+                                  ldd,
+                                  std::is_same<LayoutD, row_major>::value ? mem_row_major
+                                                                          : mem_col_major);
             }
         }
     }
