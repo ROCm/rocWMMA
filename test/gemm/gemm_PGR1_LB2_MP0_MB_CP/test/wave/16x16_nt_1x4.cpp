662e3985d512b786705fdae7464699f3e0831a02
Chris Millette <chris.millette@amd.com>
2022-08-18 16:16:47 -0700

    First pass
    [ROCm/rocwmma commit: 5cadc1f4cbdabb5b48709b0b0f5456d03a90e1bf]

--- test/gemm/gemm_PGR1_LB2_MP0_MB_CP/test/wave/16x16_nt_1x4.cpp
+++ test/gemm/gemm_PGR1_LB2_MP0_MB_CP/test/wave/16x16_nt_1x4.cpp
@@ -37,9 +37,9 @@ namespace rocwmma
                                              TestLayoutsNT,
                                              TestLdsDataLayouts,
                                              TestGemmConfigsWaveLevel,
-                                             TestBlocks1x2);
+                                             TestBlocks1x4);
 
 } // namespace rocwmma
 
 // Instantiate kernels as a test suite
-ROCWMMA_INSTANTIATE_GEMM_GTEST_SUITE(Gemm_PGR1_LB2_MP0_MB_CP, WV_16x16_NT_1x2, rocwmma::TestParams);
+ROCWMMA_INSTANTIATE_GEMM_GTEST_SUITE(Gemm_PGR1_LB2_MP0_MB_CP, WV_16x16_NT_1x4, rocwmma::TestParams);
