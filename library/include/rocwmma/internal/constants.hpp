1d3e8f3df0e6d0f94590a79b702b223a6e51c12a
Meena Karunanidhi <Meena.Karunanidhi@amd.com>
2022-11-11 09:37:25 -0700

    Update constants for rdna

--- library/include/rocwmma/internal/constants.hpp
+++ library/include/rocwmma/internal/constants.hpp
@@ -26,10 +26,15 @@
 #ifndef ROCWMMA_CONSTANTS_HPP
 #define ROCWMMA_CONSTANTS_HPP
 
+#include "config.hpp"
+
 namespace rocwmma
 {
-
+#if defined(ROCWMMA_ARCH_MI)
     static constexpr uint32_t AMDGCN_WAVE_SIZE                   = 64u;
+#elif defined(ROCWMMA_ARCH_NAVI)
+    static constexpr uint32_t AMDGCN_WAVE_SIZE                   = 32u;
+#endif
     static constexpr uint32_t AMDGCN_REGISTER_ELEMENT_SIZE_BYTES = 4u;
     static constexpr uint32_t AMDGCN_REGISTER_SIZE_BYTES
         = AMDGCN_REGISTER_ELEMENT_SIZE_BYTES * AMDGCN_WAVE_SIZE;
@@ -38,6 +43,9 @@ namespace rocwmma
     static constexpr uint32_t AMDGCN_CACHE_LINE_SIZE_BYTES = 64u;
     static constexpr uint32_t AMDGCN_DWORD_SIZE_BYTES      = 4u;
 
+#if defined(ROCWMMA_ARCH_NAVI)
+    static constexpr uint32_t AMDGCN_CDNA_RDNA_WAVE_RATIO  = 2u;
+#endif
 } // namespace rocwmma
 
 #endif // ROCWMMA_CONSTANTS_HPP
