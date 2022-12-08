cd41129fcff970861390c4d9d294adbdb3bf3273
dlangbe <david.langbehn@amd.com>
2022-12-08 08:55:47 -0800

    Modified hipRTC gemm sample

--- samples/hipRTC_gemm.cpp
+++ samples/hipRTC_gemm.cpp
@@ -31,15 +31,14 @@
 #include <hip/hiprtc.h>
 
 #include <rocwmma/rocwmma.hpp>
+// #include "rocwmma.hpp"
 
 #include "common.hpp"
-#include "hipRTC_gemm_kernel.hpp"
+// #include "hipRTC_gemm_kernel.hpp"
 
 using rocwmma::float16_t;
 using rocwmma::float32_t;
 using rocwmma::float64_t;
-
-char const* src = source.c_str();
 
 #define HIPRTC_CALL(call) assert(call == HIPRTC_SUCCESS);
 #define HIP_CALL(call) assert(call == hipSuccess);
@@ -112,6 +111,100 @@ const int WAVE_SIZE = rocwmma::AMDGCN_WAVE_SIZE;
 const int T_BLOCK_X = 4 * WAVE_SIZE;
 const int T_BLOCK_Y = 4;
 
+std::string source = R"(
+
+#include <rocwmma/rocwmma.hpp>
+// #include "rocwmma.hpp"
+
+extern "C"
+
+// The following device kernel is a naive implementation
+// of blocked GEMM. Each wave will compute one BLOCK_M x BLOCK_N
+// output block of the M x N x K GEMM, generalized as:
+// D = alpha * (A x B) + beta * C
+//
+// In this simplified example, we assume:
+// : A is in row-major format     (M x K)
+// : B is in col-major format     (K x N)
+// : C, D are in row-major format (M x N)
+// : Multiplication is NOT in-place, output is written to D matrix
+// : No LDS required
+//
+// Note: This is a simplified implementation to demonstrate API usage in
+// context of wave-level GEMM computation, and is not optimized.
+__global__ void gemm_rocwmma_d(uint32_t         m,
+                               uint32_t         n,
+                               uint32_t         k,
+                               float16_t const* a,
+                               float16_t const* b,
+                               float32_t const* c,
+                               float32_t*       d,
+                               uint32_t         lda,
+                               uint32_t         ldb,
+                               uint32_t         ldc,
+                               uint32_t         ldd,
+                               float32_t        alpha,
+                               float32_t        beta)
+{
+    // Create frags
+    auto fragA = rocwmma::fragment<rocwmma::matrix_a,
+                                   ROCWMMA_M,
+                                   ROCWMMA_N,
+                                   ROCWMMA_K,
+                                   float16_t,
+                                   rocwmma::row_major>();
+    auto fragB = rocwmma::fragment<rocwmma::matrix_b,
+                                   ROCWMMA_M,
+                                   ROCWMMA_N,
+                                   ROCWMMA_K,
+                                   float16_t,
+                                   rocwmma::col_major>();
+    auto fragC
+        = rocwmma::fragment<rocwmma::accumulator, ROCWMMA_M, ROCWMMA_N, ROCWMMA_K, float32_t>();
+    auto fragAcc
+        = rocwmma::fragment<rocwmma::accumulator, ROCWMMA_M, ROCWMMA_N, ROCWMMA_K, float32_t>();
+
+    rocwmma::fill_fragment(fragAcc, 0.0f);
+
+    // Tile using a 2D grid
+    auto majorWarp = (blockIdx.x * blockDim.x + threadIdx.x) / WAVE_SIZE;
+    auto minorWarp = (blockIdx.y * blockDim.y + threadIdx.y);
+
+    // Target C block
+    auto cRow = majorWarp * ROCWMMA_M;
+    auto cCol = minorWarp * ROCWMMA_N;
+
+    // Bounds check
+    if(cRow < m && cCol < n)
+    {
+        // fragAcc = A x B
+        for(int i = 0; i < k; i += ROCWMMA_K)
+        {
+            // Load the inputs
+            rocwmma::load_matrix_sync(fragA, a + (cRow * lda + i), lda);
+            rocwmma::load_matrix_sync(fragB, b + (i + cCol * ldb), ldb);
+
+            // Matrix multiply - accumulate using MFMA units
+            rocwmma::mma_sync(fragAcc, fragA, fragB, fragAcc);
+        }
+
+        // Fetch C matrix
+        rocwmma::load_matrix_sync(fragC, c + (cRow * ldc + cCol), ldc, rocwmma::mem_row_major);
+
+        // D = alpha * A x B + beta * C
+        for(int i = 0; i < fragC.num_elements; ++i)
+        {
+            fragC.x[i] = alpha * fragAcc.x[i] + beta * fragC.x[i];
+        }
+
+        // Store to D
+        rocwmma::store_matrix_sync(d + (cRow * ldd + cCol), fragC, ldd, rocwmma::mem_row_major);
+    }
+}
+)";
+
+char const* src = source.c_str();
+
 int main()
 {
     // gemm parameters
@@ -128,9 +221,11 @@ int main()
     hiprtcResult result;
     hiprtcResult logResult;
     const char*  opts[]
-        = {"-D__HIP_PLATFORM_AMD__", "-I/WMMA"}; // Include direct path to rocWMMA installation
+        = {"-D__HIP_PLATFORM_AMD__",
+           "-I/WMMA/samples",
+           "-I../library/include/rocwmma"}; // Include direct path to rocWMMA installation
 
-    result = hiprtcCompileProgram(prog, 2, opts); // TODO: fix segfault here
+    result = hiprtcCompileProgram(prog, 3, opts); // TODO: fix segfault here
     if(result != HIPRTC_SUCCESS)
     {
         std::cout << "HipRTC compile failed." << std::endl;
@@ -253,7 +348,7 @@ int main()
                                    0,
                                    nullptr,
                                    nullptr,
-                                   (void**)&extra));
+                                   (void**)&config));
 
     CHECK_HIP_ERROR(hipEventRecord(stopEvent));
 
