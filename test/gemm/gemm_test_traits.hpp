7678f39c892e317bd6bd40b598741431999e46c0
Chris <cmillett@cgy-metang.amd.com>
2022-12-09 12:04:43 -0700

    Add gemm test meta data and runtime dispatching helpers
    [ROCm/rocwmma commit: 8bbd2a19dff00c2b3f2649d8b8a3f290ca92036e]

---
+++ test/gemm/gemm_test_traits.hpp
@@ -0,0 +1,106 @@
+/*******************************************************************************
+ *
+ * MIT License
+ *
+ * Copyright 2021-2022 Advanced Micro Devices, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *******************************************************************************/
+
+#ifndef ROCWMMA_GEMM_TEST_TRAITS_HPP
+#define ROCWMMA_GEMM_TEST_TRAITS_HPP
+
+// The testing interface instantiates fp64 typed tests for all
+// target devices. MI-100 mfma needs to be instantiated at compile time,
+// but it doesn't do anything except provide a deprecation warning (e.g. not supported).
+// A run-time check will abort the MI-100 fp64 tests anyway.
+// Silence this warning for MmaSyncTests, as test coverage is needed
+// for fp64 on all other targets which succeed MI-100.
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#include <rocwmma/rocwmma.hpp>
+#pragma GCC diagnostic pop
+
+namespace rocwmma
+{
+    template <uint32_t BlockM,
+              uint32_t BlockN,
+              uint32_t BlockK,
+              typename InputT,
+              typename OutputT,
+              typename ComputeT,
+              uint32_t BlocksX,
+              uint32_t BlocksY,
+              uint32_t WaveSize,
+              uint32_t ArchId>
+    struct GemmTestTraits
+    {
+        // Size properties of gemm tiles
+        enum struct Sizes : uint32_t
+        {
+            TileAX    = BlocksX * BlockM,
+            TileAY    = BlockK,
+            TileASize = TileAX * TileAY * sizeof(InputT),
+
+            TileBX    = BlockK,
+            TileBY    = BlocksY * BlockN,
+            TileBSize = TileBX * TileBY * sizeof(InputT),
+
+            TileCX    = BlocksX * BlockM,
+            TileCY    = BlocksY * BlockN,
+            TileCSize = TileCX * TileCY * sizeof(ComputeT),
+
+            TileDX    = BlocksX * BlockM,
+            TileDY    = BlocksY * BlockN,
+            TileDSize = TileDX * TileDY * sizeof(OutputT),
+        };
+
+        // Tile costs
+        enum struct Cost : uint32_t
+        {
+            Granularity = 16u,
+            DWord       = 4u,
+
+            TileA = ceilDiv((uint32_t)Sizes::TileASize, Granularity* WaveSize* DWord) * Granularity,
+            TileB = ceilDiv((uint32_t)Sizes::TileBSize, Granularity* WaveSize* DWord) * Granularity,
+            TileC = ceilDiv((uint32_t)Sizes::TileCSize, Granularity* WaveSize* DWord) * Granularity,
+            TileD = ceilDiv((uint32_t)Sizes::TileDSize, Granularity* WaveSize* DWord) * Granularity,
+        };
+
+        // Architecture we are testing
+        enum Arch : bool
+        {
+            IsWave32 = (WaveSize == Constants::AMDGCN_WAVE_SIZE_32),
+            IsWave64 = (WaveSize == Constants::AMDGCN_WAVE_SIZE_64),
+
+            IsGfx908  = (ArchId == Constants::AMDGCN_ARCH_ID_GFX908),
+            IsGfx90A  = (ArchId == Constants::AMDGCN_ARCH_ID_GFX90A),
+            IsGfx1100 = (ArchId == Constants::AMDGCN_ARCH_ID_GFX1100),
+            IsGfx1101 = (ArchId == Constants::AMDGCN_ARCH_ID_GFX1101),
+            IsGfx1102 = (ArchId == Constants::AMDGCN_ARCH_ID_GFX1102),
+
+            IsGfx9  = IsGfx908 || IsGfx90A,
+            IsGfx11 = IsGfx1100 || IsGfx1101 || IsGfx1102,
+        };
+    };
+
+} // namespace rocWMMA
+
+#endif // ROCWMMA_GEMM_TEST_TRAITS_HPP
