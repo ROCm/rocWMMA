302971209f4dd7c12568634feee4f633f07207f5
Christopher Millette <63608002+cgmillette@users.noreply.github.com>
2023-01-23 15:10:10 -0700

    Unit test restructure (#132)
    * Move unit test files to their new homes
    
    * Add individual test CMakeLists files
    
    [ROCm/rocwmma commit: 342e3bf8667d2c2d91c19675551862695654988a]

---
+++ test/unit/load_store_matrix_coop_sync_test/detail/load_store_matrix_coop_sync.hpp
@@ -0,0 +1,90 @@
+/*******************************************************************************
+ *
+ * MIT License
+ *
+ * Copyright 2021-2023 Advanced Micro Devices, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *******************************************************************************/
+
+#ifndef ROCWMMA_DETAIL_LOAD_STORE_MATRIX_COOP_SYNC_HPP
+#define ROCWMMA_DETAIL_LOAD_STORE_MATRIX_COOP_SYNC_HPP
+
+#include "load_store_matrix_sync_test/detail/load_store_matrix_sync.hpp"
+#include "device/load_store_matrix_coop_sync.hpp"
+
+namespace rocwmma
+{
+
+    template <uint32_t BlockM, uint32_t BlockN, typename DataT, typename Layout>
+    struct LoadStoreMatrixCoopSyncKernelA final
+        : public LoadStoreMatrixSyncKernel<BlockM, BlockN, DataT, Layout>
+    {
+    private:
+        using Base = LoadStoreMatrixSyncKernel<BlockM, BlockN, DataT, Layout>;
+
+    protected:
+        typename Base::KernelFunc kernelImpl() const final
+        {
+            return
+                typename Base::KernelFunc(LoadStoreMatrixCoopSyncA<BlockM, BlockN, DataT, Layout>);
+        }
+    };
+
+    template <uint32_t BlockM, uint32_t BlockN, typename DataT, typename Layout>
+    struct LoadStoreMatrixCoopSyncKernelB final
+        : public LoadStoreMatrixSyncKernel<BlockM, BlockN, DataT, Layout>
+    {
+    private:
+        using Base = LoadStoreMatrixSyncKernel<BlockM, BlockN, DataT, Layout>;
+
+    protected:
+        typename Base::KernelFunc kernelImpl() const final
+        {
+            return
+                typename Base::KernelFunc(LoadStoreMatrixCoopSyncB<BlockM, BlockN, DataT, Layout>);
+        }
+    };
+
+    template <uint32_t BlockM, uint32_t BlockN, typename DataT, typename Layout>
+    struct LoadStoreMatrixCoopSyncKernelAcc final
+        : public LoadStoreMatrixSyncKernel<BlockM, BlockN, DataT, Layout>
+    {
+    private:
+        using Base = LoadStoreMatrixSyncKernel<BlockM, BlockN, DataT, Layout>;
+
+    protected:
+        typename Base::KernelFunc kernelImpl() const final
+        {
+            return typename Base::KernelFunc(
+                LoadStoreMatrixCoopSyncAcc<BlockM, BlockN, DataT, Layout>);
+        }
+    };
+
+    using LoadStoreMatrixCoopSyncGeneratorA
+        = LoadStoreMatrixSyncGenerator<LoadStoreMatrixCoopSyncKernelA>;
+    using LoadStoreMatrixCoopSyncGeneratorB
+        = LoadStoreMatrixSyncGenerator<LoadStoreMatrixCoopSyncKernelB>;
+    using LoadStoreMatrixCoopSyncGeneratorAcc
+        = LoadStoreMatrixSyncGenerator<LoadStoreMatrixCoopSyncKernelAcc>;
+
+} // namespace rocwmma
+
+#endif // ROCWMMA_DETAIL_LOAD_STORE_MATRIX_COOP_SYNC_HPP
