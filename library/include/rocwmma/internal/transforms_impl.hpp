c6566351779398757c51a13e11fdcf0e35ac17ce
Christopher Millette <chris.millette@amd.com>
2023-03-10 17:55:08 -0700

    Add optimized unpackLoHi

--- library/include/rocwmma/internal/transforms_impl.hpp
+++ library/include/rocwmma/internal/transforms_impl.hpp
@@ -327,8 +327,8 @@ namespace rocwmma
     ROCWMMA_DEVICE static inline auto unpackLo2(VecT<DataT, VecSize> const& v)
     {
         static_assert(VecSize % 2 == 0, "VecSize must be a multiple of 2");
-        using Packer   = Pack<DataT, VecSize / 2u>;
-        using Unpacker = Unpack<DataT, VecSize / 4u>;
+        using Packer   = Pack<DataT>;
+        using Unpacker = Unpack<DataT>;
         return Unpacker::exec(Blend::Zip2::exec(
             Packer::exec(extractEven(v)), Dpp::RotateR16<2>::exec(Packer::exec(extractOdd(v)))));
     }
@@ -337,8 +337,8 @@ namespace rocwmma
     ROCWMMA_DEVICE static inline auto unpackLo4(VecT<DataT, VecSize> const& v)
     {
         static_assert(VecSize % 2 == 0, "VecSize must be a multiple of 2");
-        using Packer   = Pack<DataT, VecSize / 2u>;
-        using Unpacker = Unpack<DataT, VecSize / 4u>;
+        using Packer   = Pack<DataT>;
+        using Unpacker = Unpack<DataT>;
         return Unpacker::exec(Dpp::template RotateR16<4, 0xF, 0xA>::exec(
             Packer::exec(extractOdd(v)), Packer::exec(extractEven(v))));
     }
@@ -347,8 +347,8 @@ namespace rocwmma
     ROCWMMA_DEVICE static inline auto unpackLo8(VecT<DataT, VecSize> const& v)
     {
         static_assert(VecSize % 2 == 0, "VecSize must be a multiple of 2");
-        using Packer   = Pack<DataT, VecSize / 2u>;
-        using Unpacker = Unpack<DataT, VecSize / 4u>;
+        using Packer   = Pack<DataT>;
+        using Unpacker = Unpack<DataT>;
         return Unpacker::exec(Dpp::template RotateR16<8, 0xF, 0xC>::exec(
             Packer::exec(extractOdd(v)), Packer::exec(extractEven(v))));
     }
@@ -357,8 +357,8 @@ namespace rocwmma
     ROCWMMA_DEVICE static inline auto unpackHi2(VecT<DataT, VecSize> const& v)
     {
         static_assert(VecSize % 2 == 0, "VecSize must be a multiple of 2");
-        using Packer   = Pack<DataT, VecSize / 2u>;
-        using Unpacker = Unpack<DataT, VecSize / 4u>;
+        using Packer   = Pack<DataT>;
+        using Unpacker = Unpack<DataT>;
         return Unpacker::exec(Blend::Zip2::exec(
             Dpp::RotateR16<14>::exec(Packer::exec(extractEven(v))), Packer::exec(extractOdd(v))));
     }
@@ -367,8 +367,8 @@ namespace rocwmma
     ROCWMMA_DEVICE static inline auto unpackHi4(VecT<DataT, VecSize> const& v)
     {
         static_assert(VecSize % 2 == 0, "VecSize must be a multiple of 2");
-        using Packer   = Pack<DataT, VecSize / 2u>;
-        using Unpacker = Unpack<DataT, VecSize / 4u>;
+        using Packer   = Pack<DataT>;
+        using Unpacker = Unpack<DataT>;
         return Unpacker::exec(Dpp::template RotateR16<12, 0xF, 0x5>::exec(
             Packer::exec(extractEven(v)), Packer::exec(extractOdd(v))));
     }
@@ -377,28 +377,73 @@ namespace rocwmma
     ROCWMMA_DEVICE static inline auto unpackHi8(VecT<DataT, VecSize> const& v)
     {
         static_assert(VecSize % 2 == 0, "VecSize must be a multiple of 2");
-        using Packer   = Pack<DataT, VecSize / 2u>;
-        using Unpacker = Unpack<DataT, VecSize / 4u>;
+        using Packer   = Pack<DataT>;
+        using Unpacker = Unpack<DataT>;
         return Unpacker::exec(Dpp::template RotateR16<8, 0xF, 0x3>::exec(
             Packer::exec(extractEven(v)), Packer::exec(extractOdd(v))));
+    }
+
+    template <typename DataT, uint32_t VecSize>
+    ROCWMMA_DEVICE static inline auto unpackLoHi2(VecT<DataT, VecSize> const& v)
+    {
+        static_assert(VecSize % 2 == 0, "VecSize must be a multiple of 2");
+        using Packer   = Pack<DataT>;
+        using Unpacker = Unpack<DataT>;
+
+        auto evens = Packer::exec(extractEven(v));
+        auto odds  = Packer::exec(extractOdd(v));
+        auto lo    = Blend::Zip2::exec(evens, Dpp::RotateR16<2>::exec(odds));
+        auto hi    = Blend::Zip2::exec(Dpp::RotateR16<14>::exec(evens), odds);
+
+        return Unpacker::exec(concat(lo, hi));
+    }
+
+    template <typename DataT, uint32_t VecSize>
+    ROCWMMA_DEVICE static inline auto unpackLoHi4(VecT<DataT, VecSize> const& v)
+    {
+        static_assert(VecSize % 2 == 0, "VecSize must be a multiple of 2");
+        using Packer   = Pack<DataT>;
+        using Unpacker = Unpack<DataT>;
+
+        auto evens = Packer::exec(extractEven(v));
+        auto odds  = Packer::exec(extractOdd(v));
+        auto lo    = Dpp::template RotateR16<4, 0xF, 0xA>::exec(odds, evens);
+        auto hi    = Dpp::template RotateR16<12, 0xF, 0x5>::exec(evens, odds);
+
+        return Unpacker::exec(concat(lo, hi));
+    }
+
+    template <typename DataT, uint32_t VecSize>
+    ROCWMMA_DEVICE static inline auto unpackLoHi8(VecT<DataT, VecSize> const& v)
+    {
+        static_assert(VecSize % 2 == 0, "VecSize must be a multiple of 2");
+        using Packer   = Pack<DataT>;
+        using Unpacker = Unpack<DataT>;
+
+        auto evens = Packer::exec(extractEven(v));
+        auto odds  = Packer::exec(extractOdd(v));
+        auto lo    = Dpp::template RotateR16<8, 0xF, 0xC>::exec(odds, evens);
+        auto hi    = Dpp::template RotateR16<8, 0xF, 0x3>::exec(evens, odds);
+
+        return Unpacker::exec(concat(lo, hi));
     }
 
     template <typename DataT>
     ROCWMMA_DEVICE static inline auto aos_soa_16xk_b32(VecT<DataT, 8> const& v)
     {
         // Step 1 : Unpack groups of 2
-        auto result = concat(unpackLo2(v), unpackHi2(v));
+        auto result = unpackLoHi2(v);
 
         // Step 2 : Unpack groups of 4
-        result = concat(unpackLo4(result), unpackHi4(result));
+        result = unpackLoHi4(result);
 
         // Step 3 : Unpack groups of 8
-        result = concat(unpackLo8(result), unpackHi8(result));
+        result = unpackLoHi8(result);
 
-        using Packer   = Pack<DataT, 8>;
-        using Unpacker = Unpack<DataT, 4>;
+        using Packer   = Pack<DataT>;
+        using Unpacker = Unpack<DataT>;
 
-        // Step 4 : Swizzle
+        // Step 4 : Gather
         return Unpacker::exec(Permute::Gather16<8, 0>::exec(Packer::exec(result)));
     }
 
@@ -417,102 +462,32 @@ namespace rocwmma
     template <typename DataT>
     ROCWMMA_DEVICE static inline auto aos_soa_32xk_b32(VecT<DataT, 8>& v)
     {
-        return 0;
-        // // Step 1 : Shift and mix groups of 4
-        // {
-        //     using DppRotateR16_4_0xF_0xA  = Dpp<DppImpl::Ops::RotateR16<4>, 0xF, 0xA>;
-        //     using DppRotateR16_12_0xF_0x5 = Dpp<DppImpl::Ops::RotateR16<12>, 0xF, 0x5>;
+        using Packer   = Pack<DataT>;
+        using Unpacker = Unpack<DataT>;
 
-        //     auto v0 = get<0>(v);
-        //     auto v1 = get<1>(v);
-        //     auto v2 = get<2>(v);
-        //     auto v3 = get<3>(v);
-        //     auto v4 = get<4>(v);
-        //     auto v5 = get<5>(v);
-        //     auto v6 = get<6>(v);
-        //     auto v7 = get<7>(v);
+        // Step 1 : Unpack groups of 4
+        auto result = unpackLoHi4(v);
 
-        //     get<0>(v) = DppRotateR16_4_0xF_0xA::exec(v1, v0);
-        //     get<1>(v) = DppRotateR16_12_0xF_0x5::exec(v0, v1);
-        //     get<2>(v) = DppRotateR16_4_0xF_0xA::exec(v3, v2);
-        //     get<3>(v) = DppRotateR16_12_0xF_0x5::exec(v2, v3);
-        //     get<4>(v) = DppRotateR16_4_0xF_0xA::exec(v5, v4);
-        //     get<5>(v) = DppRotateR16_12_0xF_0x5::exec(v4, v5);
-        //     get<6>(v) = DppRotateR16_4_0xF_0xA::exec(v7, v6);
-        //     get<7>(v) = DppRotateR16_12_0xF_0x5::exec(v6, v7);
-        // }
+        // Step 2 : Unpack groups of 8
+        result = unpackLoHi8(result);
 
-        // // Step 2: Shift and mix groups of 8
-        // {
-        //     using DppRotateR16_8_0xF_0xC = Dpp<DppImpl::Ops::RotateR16<8>, 0xF, 0xC>;
-        //     using DppRotateR16_8_0xF_0x3 = Dpp<DppImpl::Ops::RotateR16<8>, 0xF, 0x3>;
+        // Step 3 : Unpack groups of 16
+        // In order to save some operations, we can
+        // rotate the lo components only and rotate the
+        // hi later with the gather offset.
+        auto evens = Packer::exec(extractEven(v));
+        auto odds  = Packer::exec(extractOdd(v));
 
-        //     auto v0 = get<0>(v);
-        //     auto v1 = get<1>(v);
-        //     auto v2 = get<2>(v);
-        //     auto v3 = get<3>(v);
-        //     auto v4 = get<4>(v);
-        //     auto v5 = get<5>(v);
-        //     auto v6 = get<6>(v);
-        //     auto v7 = get<7>(v);
+        auto rot = Swizzle::RotateR32<16>::exec(odds);
+        auto lo  = Blend::Zip16::exec(evens, rot);
+        auto hi  = Blend::Zip16::exec(rot, evens);
 
-        //     get<0>(v) = DppRotateR16_8_0xF_0xC::exec(v2, v0);
-        //     get<1>(v) = DppRotateR16_8_0xF_0xC::exec(v3, v1);
-        //     get<2>(v) = DppRotateR16_8_0xF_0x3::exec(v0, v2);
-        //     get<3>(v) = DppRotateR16_8_0xF_0x3::exec(v1, v3);
-        //     get<4>(v) = DppRotateR16_8_0xF_0xC::exec(v6, v4);
-        //     get<5>(v) = DppRotateR16_8_0xF_0xC::exec(v7, v5);
-        //     get<6>(v) = DppRotateR16_8_0xF_0x3::exec(v4, v6);
-        //     get<7>(v) = DppRotateR16_8_0xF_0x3::exec(v5, v7);
-        // }
+        // Step 4 : Gather
+        // Note the offset of 16 in hi
+        lo = Permute::Gather32<8, 0>::exec(lo);
+        hi = Permute::Gather32<8, 16>::exec(hi);
 
-        // // Step 3: Rotate latter half of the registers
-        // {
-        //     using SwzRotateR32_16 = Swizzle<SwizzleImpl::Ops::RotateR32<16>>;
-        //     SwzRotateR32_16::exec(get<4>(v));
-        //     SwzRotateR32_16::exec(get<5>(v));
-        //     SwzRotateR32_16::exec(get<6>(v));
-        //     SwzRotateR32_16::exec(get<7>(v));
-        // }
-
-        // // Step 4:
-        // {
-        //     using DppMMove_0x5_0xF = Dpp<DppImpl::Ops::MaskMove, 0x5, 0xF>;
-
-        //     auto v0 = get<0>(v);
-        //     auto v1 = get<1>(v);
-        //     auto v2 = get<2>(v);
-        //     auto v3 = get<3>(v);
-        //     auto v4 = get<4>(v);
-        //     auto v5 = get<5>(v);
-        //     auto v6 = get<6>(v);
-        //     auto v7 = get<7>(v);
-
-        //     get<0>(v) = DppMMove_0x5_0xF::exec(v0, v4);
-        //     get<1>(v) = DppMMove_0x5_0xF::exec(v1, v5);
-        //     get<2>(v) = DppMMove_0x5_0xF::exec(v2, v6);
-        //     get<3>(v) = DppMMove_0x5_0xF::exec(v3, v7);
-        //     get<4>(v) = DppMMove_0x5_0xF::exec(v4, v0);
-        //     get<5>(v) = DppMMove_0x5_0xF::exec(v5, v1);
-        //     get<6>(v) = DppMMove_0x5_0xF::exec(v6, v2);
-        //     get<7>(v) = DppMMove_0x5_0xF::exec(v7, v3);
-        // }
-
-        // // Step 5: Permute
-        // {
-        //     using Gather32_8_0 = Permute<PermuteImpl::Ops::Gather32<8, 0>>;
-        //     using Gather32_8_16 = Permute<PermuteImpl::Ops::Gather32<8, 16>>;
-
-        //     constexpr uint32_t waveSize = 64u;
-        //     Gather32_8_0::exec(get<0>(v), threadIdx.x % waveSize);
-        //     Gather32_8_0::exec(get<1>(v), threadIdx.x % waveSize);
-        //     Gather32_8_0::exec(get<2>(v), threadIdx.x % waveSize);
-        //     Gather32_8_0::exec(get<3>(v), threadIdx.x % waveSize);
-        //     Gather32_8_16::exec(get<4>(v), threadIdx.x % waveSize);
-        //     Gather32_8_16::exec(get<5>(v), threadIdx.x % waveSize);
-        //     Gather32_8_16::exec(get<6>(v), threadIdx.x % waveSize);
-        //     Gather32_8_16::exec(get<7>(v), threadIdx.x % waveSize);
-        // }
+        return Unpacker::exec(concat(lo, hi));
     }
 
     template <typename DataT>
