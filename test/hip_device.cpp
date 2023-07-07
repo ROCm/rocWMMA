a3f0439819dbec5df97add6aa249aa747f8d13d7
Meena Karunanidhi <85510608+mkarunan@users.noreply.github.com>
2023-07-07 10:31:19 -0400

    Enable rocm_smi only to benchmark (#195) (#197)
    * Enable rocm_smi only to benchmark (#195)
    
    * Store status in a var
    
    * Support error codes from smi other than Success codes
    
    * Verify all error codes

--- test/hip_device.cpp
+++ test/hip_device.cpp
@@ -77,34 +77,45 @@ namespace rocwmma
         mSharedMemSize = mProps.sharedMemPerBlock;
         mCuCount       = mProps.multiProcessorCount;
         mMaxFreqMhz    = static_cast<int>(static_cast<double>(mProps.clockRate) / 1000.0);
+        mCurFreqMhz = mMaxFreqMhz;
 
-        CHECK_RSMI_ERROR(rsmi_init(0));
-        uint64_t hipPCIID = 0;
-        hipPCIID |= mProps.pciDeviceID & 0xFF;
-        hipPCIID |= ((mProps.pciBusID & 0xFF) << 8);
-        hipPCIID |= (mProps.pciDomainID) << 16;
 
-        uint32_t smiCount = 0;
-        CHECK_RSMI_ERROR(rsmi_num_monitor_devices(&smiCount));
-
-        uint32_t m_smiDeviceIndex;
-        for(uint32_t smiIndex = 0; smiIndex < smiCount; smiIndex++)
+#ifdef ROCWMMA_BENCHMARK_TESTS
+        bool smiErrorFlag = false;
+        CHECK_RSMI_ERROR(rsmi_init(0), smiErrorFlag);
+        if(!smiErrorFlag)
         {
-            uint64_t rsmiPCIID = 0;
-            CHECK_RSMI_ERROR(rsmi_dev_pci_id_get(smiIndex, &rsmiPCIID));
+            uint64_t hipPCIID = 0;
+            hipPCIID |= mProps.pciDeviceID & 0xFF;
+            hipPCIID |= ((mProps.pciBusID & 0xFF) << 8);
+            hipPCIID |= (mProps.pciDomainID) << 16;
 
-            if(hipPCIID == rsmiPCIID)
+            uint32_t smiCount = 0;
+            CHECK_RSMI_ERROR(rsmi_num_monitor_devices(&smiCount), smiErrorFlag);
+            if(!smiErrorFlag)
             {
-                m_smiDeviceIndex = smiIndex;
-                break;
+                uint32_t m_smiDeviceIndex;
+                for(uint32_t smiIndex = 0; smiIndex < smiCount; smiIndex++)
+                {
+                    uint64_t rsmiPCIID = 0;
+                    CHECK_RSMI_ERROR(rsmi_dev_pci_id_get(smiIndex, &rsmiPCIID), smiErrorFlag);
+                    if(!smiErrorFlag && (hipPCIID == rsmiPCIID))
+                    {
+                        m_smiDeviceIndex = smiIndex;
+                        break;
+                    }
+                }
+
+                rsmi_frequencies_t freq;
+                CHECK_RSMI_ERROR(rsmi_dev_gpu_clk_freq_get(m_smiDeviceIndex, RSMI_CLK_TYPE_SYS, &freq), smiErrorFlag);
+                if(!smiErrorFlag)
+                {
+                    mCurFreqMhz = freq.frequency[freq.current] / 1000000;
+                }
             }
         }
-
-        rsmi_frequencies_t freq;
-        CHECK_RSMI_ERROR(rsmi_dev_gpu_clk_freq_get(m_smiDeviceIndex, RSMI_CLK_TYPE_SYS, &freq));
-
-        mCurFreqMhz = freq.frequency[freq.current] / 1000000;
-}
+#endif
+    }
 
     hipDevice_t HipDevice::getDeviceHandle() const
     {
@@ -153,7 +164,10 @@ namespace rocwmma
 
     HipDevice::~HipDevice()
     {
-        CHECK_RSMI_ERROR(rsmi_shut_down());
+#ifdef ROCWMMA_BENCHMARK_TESTS
+        bool smiErrorFlag = false;
+        CHECK_RSMI_ERROR(rsmi_shut_down(), smiErrorFlag);
+#endif
     }
 
     // Need to check the host device target support statically before hip modules attempt
