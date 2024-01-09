708faf9152ab93b5bd708a42b8d60b4fe688e68e
Chris Millette <chris.millette@amd.com>
2024-01-09 18:22:23 +0000

    Add unit test for pack util
    [ROCm/rocwmma commit: 358263a4d5e26b9df6be84c8a519d02f2db594de]

---
+++ test/unit/pack_util_test/detail/pack_util.hpp
@@ -0,0 +1,106 @@
+/*******************************************************************************
+ *
+ * MIT License
+ *
+ * Copyright (C) 2021-2024 Advanced Micro Devices, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *******************************************************************************/
+
+#ifndef ROCWMMA_DETAIL_PACK_UTIL_TEST_HPP
+#define ROCWMMA_DETAIL_PACK_UTIL_TEST_HPP
+
+#include "device/pack_util.hpp"
+#include "unit_kernel_base.hpp"
+
+namespace rocwmma
+{
+
+    // Wrapper into the actual device function
+    template <uint32_t VecSize, typename DataT>
+    struct PackUtilKernel final
+        : public UnitKernelBase<1,
+                                1,
+                                DataT,
+                                col_major> // BlockM, BlockN, DataLayout are redundant for this test
+    {
+    private:
+        using Base = UnitKernelBase<1, 1, DataT, col_major>;
+
+    public:
+        PackUtilKernel()        = default;
+        ~PackUtilKernel() final = default;
+
+        void setupImpl(typename Base::DataStorage::ProblemSize const& probsize) final
+        {
+            // Need at least 1 element for the result
+            auto& dataInstance = Base::DataStorage::instance();
+            dataInstance->resizeStorage(probsize);
+
+            dataInstance->hostOut().get()[0] = static_cast<DataT>(ERROR_VALUE);
+            dataInstance->copyData(dataInstance->deviceOut(), dataInstance->hostOut(), 1);
+        }
+
+        void validateResultsImpl() final
+        {
+            auto& dataInstance = Base::DataStorage::instance();
+
+            // Cache current kernel result from device
+            dataInstance->copyData(dataInstance->hostOut(), dataInstance->deviceOut(), 1);
+
+            // Check the single output result
+            Base::mValidationResult = (dataInstance->hostOut().get()[0] == DataT(SUCCESS_VALUE));
+        }
+
+        typename Base::KernelFunc kernelImpl() const final
+        {
+            return typename Base::KernelFunc(packUtilTest<DataT, VecSize>);
+        }
+    };
+
+    // This is the GeneratorImpl class
+    struct PackUtilGenerator
+    {
+        // Indices to test parameters
+        enum : uint32_t
+        {
+            VecSize = 0,
+            DataT   = 1,
+        };
+
+        using ResultT = std::shared_ptr<KernelI>;
+
+        template <typename... Ts>
+        static ResultT generate(std::tuple<Ts...> testParams)
+        {
+            // Map GTest params to Kernel params
+            using TestParamsT = std::tuple<Ts...>;
+            using KernelT
+                = PackUtilKernel<std::tuple_element_t<VecSize, TestParamsT>::value, // VecSize
+                                 std::tuple_element_t<DataT, TestParamsT> // DataT
+                                 >;
+
+            return std::make_shared<KernelT>();
+        }
+    };
+
+} // namespace rocwmma
+
+#endif // ROCWMMA_DETAIL_PACK_UTIL_TEST_HPP
