39305ea3b21ea17d78d3bac497ad0c5e3f7b7c70
Cong Ma <congma13@amd.com>
2024-01-19 22:04:06 +0000

    Add unit test for transfroms

---
+++ test/unit/transforms_test/test/transforms.cpp
@@ -0,0 +1,106 @@
+/*******************************************************************************
+ *
+ * MIT License
+ *
+ * Copyright (C) 2021-2024 Advanced Micro Devices, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *******************************************************************************/
+
+#include <tuple>
+#include <type_traits>
+
+#include "detail/transforms.hpp"
+#include "kernel_generator.hpp"
+#include "unit_test.hpp"
+
+namespace rocwmma
+{
+
+    struct TestParams : public UnitTestParams
+    {
+        using Base = UnitTestParams;
+
+        // Types: Base IOC + double
+        using Types = std::tuple<
+            int8_t, // use int8_t since float8_t will be skipped by default on some platform
+            float16_t,
+            float32_t,
+            float64_t>;
+
+        // Vector Width.
+        // using VWs = std::tuple<I<2>, I<4>, I<8>>;
+        using VWs = std::tuple<I<4>>;
+
+        // size of K dimension
+        using K = std::tuple<I<16>, I<32>, I<64>, I<128>, I<256>>;
+
+        using KernelParams = typename CombineLists<K, VWs, Types>::Result;
+
+        // Assemble the kernel generator
+        // Kernel: VectorUtil
+        using GeneratorImpl   = TransformsGenerator;
+        using KernelGenerator = KernelGenerator<KernelParams, GeneratorImpl>;
+
+        // Sanity check for kernel generator
+        static_assert(std::is_same<typename GeneratorImpl::ResultT, typename Base::KernelT>::value,
+                      "Kernels from this generator do not match testing interface");
+
+        static inline std::vector<ThreadBlockT> threadBlocks()
+        {
+            auto warpSize = HipDevice::instance()->warpSize();
+            // clang-format off
+            return { {warpSize, 1} };
+            // clang-format on
+        }
+
+        static inline std::vector<ProblemSizeT> problemSizes()
+        {
+            // clang-format off
+            return { {1, 1} };
+            // clang-format on
+        }
+
+        static inline typename KernelGenerator::ResultT kernels()
+        {
+            return KernelGenerator::generate();
+        }
+    };
+
+} // namespace rocwmma
+
+// Test suite for unique parameterization
+class TransfromsTest : public rocwmma::UnitTest
+{
+};
+
+TEST_P(TransfromsTest, RunKernel)
+{
+    this->RunKernel();
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    KernelTests,
+    TransfromsTest,
+    ::testing::Combine(::testing::ValuesIn(rocwmma::TestParams::kernels()),
+                       ::testing::ValuesIn(rocwmma::TestParams::threadBlocks()),
+                       ::testing::ValuesIn(rocwmma::TestParams::problemSizes()),
+                       ::testing::ValuesIn(rocwmma::TestParams::param1s()),
+                       ::testing::ValuesIn(rocwmma::TestParams::param2s())));
