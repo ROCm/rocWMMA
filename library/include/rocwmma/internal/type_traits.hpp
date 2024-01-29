e9b9f6bcf318891775ce554ab62c3290a2daada3
dlangbe <dlangbeh@amd.com>
2024-01-29 18:34:50 -0500

    type traits cleanup

--- library/include/rocwmma/internal/type_traits.hpp
+++ library/include/rocwmma/internal/type_traits.hpp
@@ -506,30 +506,27 @@ namespace NUMERIC_LIMITS_NAMESPACE
 namespace rocwmma
 {
 #if !defined(__HIPCC_RTC__)
-    template <typename T, typename std::enable_if_t<std::is_integral<T>::value, int> = 0>
+    template <typename T, enable_if_t<is_integral<T>::value, int> = 0>
     constexpr auto maxExactInteger() -> decltype(numeric_limits<T>::max())
     {
         return numeric_limits<T>::max();
     }
 
     template <typename T,
-              typename std::
-                  enable_if_t<std::is_floating_point<T>::value && numeric_limits<T>::digits, int>
-              = 0>
-    constexpr auto maxExactInteger() ->
-        typename std::conditional_t<std::is_same<T, float64_t>::value, int64_t, int32_t>
+              enable_if_t<is_floating_point<T>::value && numeric_limits<T>::digits, int> = 0>
+    constexpr auto maxExactInteger()
+        -> conditional_t<is_same<T, float64_t>::value, int64_t, int32_t>
     {
-        using RetT =
-            typename std::conditional_t<std::is_same<T, float64_t>::value, int64_t, int32_t>;
+        using RetT = conditional_t<is_same<T, float64_t>::value, int64_t, int32_t>;
         return ((RetT)1 << numeric_limits<T>::digits);
     }
 
     template <typename T,
-              typename std::enable_if_t<
+              enable_if_t<
 #if !ROCWMMA_NO_HALF
-                  std::is_same<T, hfloat16_t>::value ||
+                  is_same<T, hfloat16_t>::value ||
 #endif // !ROCWMMA_NO_HALF
-                      std::is_same<T, float16_t>::value,
+                      is_same<T, float16_t>::value,
                   int>
               = 0>
     constexpr auto maxExactInteger() -> int32_t
@@ -538,29 +535,28 @@ namespace rocwmma
         return ((int32_t)1 << 11);
     }
 
-    template <typename T, typename std::enable_if_t<std::is_same<T, bfloat16_t>::value, int> = 0>
+    template <typename T, enable_if_t<is_same<T, bfloat16_t>::value, int> = 0>
     constexpr auto maxExactInteger() -> int32_t
     {
         // b16 mantissa is 7 bits
         return ((int32_t)1 << 8);
     }
 
-    template <typename T,
-              typename std::enable_if_t<std::is_same<T, rocwmma::float8_t>::value, int> = 0>
+    template <typename T, enable_if_t<is_same<T, rocwmma::float8_t>::value, int> = 0>
     constexpr auto maxExactInteger() -> int32_t
     {
         // f8 mantissa is 3 bits
         return ((int32_t)1 << 4);
     }
 
-    template <typename T, typename std::enable_if_t<std::is_same<T, bfloat8_t>::value, int> = 0>
+    template <typename T, enable_if_t<is_same<T, bfloat8_t>::value, int> = 0>
     constexpr auto maxExactInteger() -> int32_t
     {
         // bf8 mantissa is 2 bits
         return ((int32_t)1 << 3);
     }
 
-    template <typename T, typename std::enable_if_t<std::is_same<T, xfloat32_t>::value, int> = 0>
+    template <typename T, enable_if_t<is_same<T, xfloat32_t>::value, int> = 0>
     constexpr auto maxExactInteger() -> int32_t
     {
         // xf32 mantissa is 7 bits
