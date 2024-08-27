8e4327b8b52347bba2734b452651bbc86200d214
Cong Ma <congma13@amd.com>
2024-08-27 20:56:00 +0000

    Add tests of DPP Rotate

--- test/unit/cross_lane_ops_test/test/dpp_bcast_4.cpp
+++ test/unit/cross_lane_ops_test/test/dpp_bcast_4.cpp
@@ -41,7 +41,10 @@ namespace rocwmma
         // Types: Base IOC + double
         using Types = typename Base::TestAllSizeTypes;
 
-        using DppOps = std::tuple<DppImpl::Ops::BCast4<0>, DppImpl::Ops::BCast4<3>>;
+        using DppOps = std::tuple<DppImpl::Ops::BCast4<0>,
+                                  DppImpl::Ops::BCast4<1>,
+                                  DppImpl::Ops::BCast4<2>,
+                                  DppImpl::Ops::BCast4<3>>;
 
         // Test random assortment of banks and rows
         using WriteRowMasks  = std::tuple<I<0xF>, I<0x5>, I<0xA>>;
