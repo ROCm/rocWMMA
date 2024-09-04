05439be5cca3a7f78895791483e862c42d63fdb5
Cong Ma <congma13@amd.com>
2024-09-04 11:30:58 -0600

    Add permute wave rotate check
    [ROCm/rocwmma commit: bf329d25ea7bee23581a646e374bfcf3c0fb5c8b]

--- test/unit/cross_lane_ops_test/detail/cross_lane_ops.hpp
+++ test/unit/cross_lane_ops_test/detail/cross_lane_ops.hpp
@@ -97,8 +97,18 @@ namespace rocwmma
                     && (CrossLaneOp::opImpl() == CrossLaneOps::Properties::OP_IMPL_DPP)
                     && (CrossLaneOp::opId() == CrossLaneOps::Properties::OP_ID_WFALL_BCAST));
 
+            bool permuteWaveRotateCheck
+                = !((isGfx11 || isGfx12)
+                    && (CrossLaneOp::opImpl() == CrossLaneOps::Properties::OP_IMPL_PERMUTE ||
+                        CrossLaneOp::opImpl() == CrossLaneOps::Properties::OP_IMPL_BPERMUTE
+                        )
+                    && (CrossLaneOp::opId() == CrossLaneOps::Properties::OP_ID_ROTATE)
+                    && (CrossLaneOp::groupSize() == CrossLaneOps::Properties::OP_GROUP_SIZE_WARP));
+
+            printf("permuteWaveRotateCheck %d\n", permuteWaveRotateCheck);
+
             return Base::checkDevice() && dppBCast16Check && dppWaveShiftCheck && dppWaveRotateCheck
-                   && dppWaterfallBCastCheck;
+                   && dppWaterfallBCastCheck && permuteWaveRotateCheck;
         }
 
         std::ostream& printHeader(std::ostream& stream = std::cout) const final
