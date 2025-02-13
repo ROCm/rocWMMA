1617bc3224c18adaac81f3344fe9143579c7c4ef
Chris Millette <chris.millette@amd.com>
2025-02-13 22:47:46 +0000

    Use Stride instead of Scale
    [ROCm/rocwmma commit: 01824919f2dbff4685307d6a5894fc977beea24c]

--- library/include/rocwmma/internal/utility/sequence_impl.hpp
+++ library/include/rocwmma/internal/utility/sequence_impl.hpp
@@ -93,17 +93,17 @@ namespace rocwmma
         template <size_t N>
         using make_index_sequence = make_integer_sequence<size_t, N>;
 
-        template <size_t Offset, size_t Scale, size_t... Is>
+        template <size_t Offset, size_t Stride, size_t... Is>
         ROCWMMA_HOST_DEVICE constexpr inline auto
             make_offset_index_sequence_helper(index_sequence<Is...>)
         {
-            return index_sequence<(Offset + Is * Scale)...>{};
+            return index_sequence<(Offset + Is * Stride)...>{};
         }
 
-        template <size_t N, size_t Offset = 0u, size_t Scale = 1u>
+        template <size_t N, size_t Offset = 0u, size_t Stride = 1u>
         ROCWMMA_HOST_DEVICE constexpr inline auto make_offset_index_sequence()
         {
-            return make_offset_index_sequence_helper<Offset, Scale>(make_index_sequence<N>{});
+            return make_offset_index_sequence_helper<Offset, Stride>(make_index_sequence<N>{});
         }
 
     } // namespace detail
