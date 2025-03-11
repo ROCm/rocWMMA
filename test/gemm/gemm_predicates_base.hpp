a5e3c0e49acae4e63e1793badc65e33ebc860f21
Chris Millette <chris.millette@amd.com>
2025-03-11 23:56:49 +0000

    Skip tests that don't allow 4 waves in interleaved tests
    [ROCm/rocwmma commit: 23e9a8ca4ca8ce4a7e8145992f302ea8d064bb31]

--- test/gemm/gemm_predicates_base.hpp
+++ test/gemm/gemm_predicates_base.hpp
@@ -177,8 +177,8 @@ namespace rocwmma
             = !((bool)TestTraits::InputType::IsFloat16 || (bool)TestTraits::InputType::IsBFloat16)
               || ((bool)TestTraits::BlockSizes::isBlockMN16 && (BlockK >= 16u)
                   && (BlockK % 16u == 0u))
-              || ((bool)TestTraits::BlockSizes::isBlockMN32 && (BlockK >= 8u)
-                  && (BlockK % 8u == 0u)),
+              || ((bool)TestTraits::BlockSizes::isBlockMN32 && (BlockK >= 16u)
+                  && (BlockK % 16u == 0u)),
 
             // Older gfx908 arch has half BlockK on bfloat16_t
             // BlockM/N = 16; Block K >= 8
@@ -196,8 +196,8 @@ namespace rocwmma
             F32BlockSizeTest = !((bool)TestTraits::InputType::IsFloat32)
                                || ((bool)TestTraits::BlockSizes::isBlockMN16 && (BlockK >= 4u)
                                    && (BlockK % 4u == 0u))
-                               || ((bool)TestTraits::BlockSizes::isBlockMN32 && (BlockK >= 4u)
-                                   && (BlockK % 4u == 0u)),
+                               || ((bool)TestTraits::BlockSizes::isBlockMN32 && (BlockK >= 8u)
+                                   && (BlockK % 8u == 0u)),
 
             // General xfloat32_t block size
             // BlockM/N = 16; Block K >= 8
